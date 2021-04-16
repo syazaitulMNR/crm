13d6ef40465f0de8ec61404d63de7b93e1cb3208
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-16 16:45:44 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -56,7 +56,7 @@
             
             <br>
             <!-- Show success payment in table ----------------------------------------------->
-            @if(count($payment) > 0)
+            @if(count($student) > 0)
             <table class="table table-hover" id="successTable">
                 <thead>
                 <tr class="header">
@@ -102,7 +102,7 @@
             @else
             <p>There are no any payment yet.</p>
             @endif
-            <div class="float-right pt-3">{{$payment->links()}}</div>   
+            <div class="float-right pt-3">{{$student->links()}}</div>   
             
           </div>
           
