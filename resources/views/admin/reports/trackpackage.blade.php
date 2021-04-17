7be941fe9fd4d993843e9f303a0b5acc2e1631b8
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-17 09:04:00 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -54,7 +54,7 @@
             <div class="row">
               @foreach ($package as $packages)
               <div class="col-md-4">
-                <a class="btn bg-dark btn-lg text-white text-center" href="{{ url('viewbypackage') }}/{{ $product->product_id }}/{{ $packages->package_id }}">
+                <a class="btn bg-dark btn-lg text-white text-center" style="width: 30%" href="{{ url('viewbypackage') }}/{{ $product->product_id }}/{{ $packages->package_id }}">
                   <h6 class="pt-3 pb-2">{{$packages->name}}</h6>
                 </a>
               </div>
