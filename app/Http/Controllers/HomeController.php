122efe124f7dc3a95d7d17bee51028aecbc66029
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-19 12:38:35 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -923,7 +923,7 @@ class HomeController extends Controller
         $time_from = '04:21AM';
         $time_to = '05:21AM';
         
-        Mail::to($email_pkg2)->send(new SendMailable($name, $package, $products, $date_from, $date_to, $time_from, $time_to));
+        \SendGrid\Mail\Mail::to($email_pkg2)->send(new SendMailable($name, $package, $products, $date_from, $date_to, $time_from, $time_to));
     }
 }
 
