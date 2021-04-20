ac5c359709b37216ff364ac002e4fcad46cddf1c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-20 14:46:46 +0800

    Update ExistCustomerController.php

--- app/Http/Controllers/ExistCustomerController.php
+++ app/Http/Controllers/ExistCustomerController.php
@@ -220,38 +220,60 @@ class ExistCustomerController extends Controller
         $product = Product::where('product_id', $product_id)->first();
         $package = Package::where('package_id', $package_id)->first();
 
-        $to_name = 'noreply@momentuminternet.com';
-        $to_email = $student->email; 
-        
-        $data['name']=$student->first_name;
-        $data['ic']=$student->ic;
-        $data['email']=$student->email;
-        $data['phoneno']=$student->phoneno;
-        $data['total']=$payment->item_total;
-        $data['quantity']=$payment->quantity;
+        $apikey = env('MAIL_PASSWORD');
+        $sendgrid = new \SendGrid($apikey);
+                
+        try {
+            $to_name = $student->first_name;
+            $to_email = $student->email; 
+            
+            $data['name']=$student->first_name;
+            $data['ic']=$student->ic;
+            $data['email']=$student->email;
+            $data['phoneno']=$student->phoneno;
+            $data['total']=$payment->item_total;
+            $data['quantity']=$payment->quantity;
 
-        $data['product']=$product->name;
-        $data['package_id']=$package->package_id;
-        $data['package']=$package->name;
-        $data['price']=$package->price;
+            $data['product']=$product->name;
+            $data['package_id']=$package->package_id;
+            $data['package']=$package->name;
+            $data['price']=$package->price;
 
-        $data['date_receive']=date('d-m-Y');
-        $data['payment_id']=$payment->payment_id;
-        $data['product_id']=$product->product_id;        
-        $data['student_id']=$student->stud_id;
-          
+            $data['date_receive']=date('d-m-Y');
+            $data['payment_id']=$payment->payment_id;
+            $data['product_id']=$product->product_id;        
+            $data['student_id']=$student->stud_id;
+            
         // $invoice = PDF::loadView('emails.invoice', $data);
         // $receipt = PDF::loadView('emails.receipt', $data);
 
         // Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email, $invoice, $receipt)
-        Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) 
-        {
-            $message->to($to_email, $to_name)->subject('Pengesahan Pembelian');
-            $message->from('noreply@momentuminternet.my','noreply');
-            // $message->attachData($invoice->output(), "Invoice.pdf");
-            // $message->attachData($receipt->output(), "Receipt.pdf");
+        // Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) 
+        // {
+        //     $message->to($to_email, $to_name)->subject('Pengesahan Pembelian');
+        //     $message->from('noreply@momentuminternet.my','noreply');
+        //     // $message->attachData($invoice->output(), "Invoice.pdf");
+        //     // $message->attachData($receipt->output(), "Receipt.pdf");
 
-        });
+        // });
+
+            $response = $sendgrid->send('emails.mail', $data, function($email) use ($to_name, $to_email) 
+            {
+                $email = new \SendGrid\Mail\Mail(); 
+                $email->setFrom("noreply@momentuminternet.my", "Momentum Internet Sdn Bhd");
+                $email->setSubject("Pengesahan Pembelian");
+                $email->addTo($to_email, $to_name);
+    
+            });
+            //print $response->statusCode() . "\n";
+            //print_r($response->headers());
+            //print $response->body() . "\n";
+
+        } catch (Exception $e) {
+
+            echo 'Caught exception: '. $e->getMessage() ."\n";
+
+        }
         /*-- End Email -----------------------------------------------------------*/
 
         $student->save();
