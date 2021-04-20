c7e0aee672c3f24e531d22ee8ffe7710adec2bd3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-20 10:37:00 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends ConsoleKernel
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
+     *
      */
     protected function schedule(Schedule $schedule)
     {
