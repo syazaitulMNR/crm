2ce28bb4a040d14656b8d65d23c2a68b80122fc8
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-22 11:12:56 +0800

    Update ExistCustomerController.php

--- app/Http/Controllers/ExistCustomerController.php
+++ app/Http/Controllers/ExistCustomerController.php
@@ -288,7 +288,7 @@ class ExistCustomerController extends Controller
         );
 
         $test = $response->toArray();
-        dd($test['url']);
+        dd($response->toArray());
 
         // return redirect($test['url']);
     }
