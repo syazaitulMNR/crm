1a717a51cb51cbb8f4eae38f036f26ab3abfe73d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-22 16:04:27 +0800

    Update ExistCustomerController.php

--- app/Http/Controllers/ExistCustomerController.php
+++ app/Http/Controllers/ExistCustomerController.php
@@ -282,7 +282,7 @@ class ExistCustomerController extends Controller
             $student->phoneno,
             $student->first_name,
             \Duit\MYR::given($payment->totalprice * 100),
-            'https://mims.momentuminternet.my',
+            'https://mims.momentuminternet.my/redirect-billplz',
             $product->name . ' - ' . $package->name,
             ['redirect_url' => 'https://mims.momentuminternet.my/redirect-billplz']
         );
