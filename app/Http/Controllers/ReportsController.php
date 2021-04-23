211698c2f674ea5fd7d4b43b3d39b0a8565710b6
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-23 11:21:19 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -79,7 +79,7 @@ class ReportsController extends Controller
 
         $total = Payment::where('product_id', $product_id)->where('package_id', $package_id)->count();
         $totalsuccess = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package_id)->count();
-        $totalcancel = Payment::where('status','cancelled')->where('product_id', $product_id)->where('package_id', $package_id)->count();
+        $totalcancel = Payment::where('status','due')->where('product_id', $product_id)->where('package_id', $package_id)->count();
         
         return view('admin.reports.viewbypackage', compact('product', 'package', 'payment', 'total', 'totalsuccess', 'totalcancel'));
     }
