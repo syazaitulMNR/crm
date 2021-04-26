bd8f95a66b126371d453bc33c6e438a3ded0ebd0
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-26 10:01:07 +0800

    Update PengesahanPembelian.php

--- app/Mail/PengesahanPembelian.php
+++ app/Mail/PengesahanPembelian.php
@@ -10,16 +10,15 @@ use Illuminate\Queue\SerializesModels;
 class PengesahanPembelian extends Mailable
 {
     use Queueable, SerializesModels;
-    protected $send_mail, $product_name, $package_name, $packageId, $payment_id, $productId, $student_id;
+    protected $product_name, $package_name, $packageId, $payment_id, $productId, $student_id;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($send_mail, $product_name, $package_name, $packageId, $payment_id, $productId, $student_id)
+    public function __construct($product_name, $package_name, $packageId, $payment_id, $productId, $student_id)
     {
-        $this->send_mail = $send_mail;
         $this->product_name = $product_name;        
         $this->package_name = $package_name;
         $this->packageId = $packageId;
