7b3529740cccb83e09ccad02661f588ff2cb379d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-27 11:07:28 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -7,7 +7,7 @@ Upgrade Pakej
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 px-3 py-5 text-center">
+        <div class="col-md-12 px-3 py-4 text-center">
             <img src="/assets/images/logo.png" style="max-width:200px">
             <h1 class="text-dark px-4 pt-3">{{ $product->name }}</h1>
             <h6>Hai! Sila buat pilihan di bawah untuk upgrade pakej.</h6>
