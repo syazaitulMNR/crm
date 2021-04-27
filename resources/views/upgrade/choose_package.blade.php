6459d5ed770055ab5f00ec0203671931ad70113f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-27 12:05:16 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -6,45 +6,12 @@ Upgrade Pakej
 
 <style>
     /* Pricing Card */
-    .pricing .cards {
+    .pricing .mySubmit {
       border: none;   
       color: dark; 
-      background: #fda90e;
-      border-radius: 1rem;
       transition: all 0.2s;
       box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.5);
     }
-  
-    .pricing hr {
-      background-color: white;
-      margin: 1.5rem 0;
-    }
-  
-    .pricing .cards-title {
-      color: dark;
-      margin: 0.5rem 0;
-      letter-spacing: .1rem;
-      font-weight: bold;
-    }
-  
-    .pricing .cards-price {
-      color: rgb(255, 0, 13);
-      font-size: 3rem;
-      margin: 0;
-    }
-  
-    .pricing .cards-price .period {
-      font-size: 0.8rem;
-    }
-  
-    .pricing ul li {
-      margin-bottom: 1rem;
-    }
-  
-    .pricing .text-muted {
-      opacity: 0.7;
-    }
-  
     .pricing .btn {
       border-radius: 5rem;
       letter-spacing: .1rem;
@@ -80,23 +47,15 @@ Upgrade Pakej
                         @else
                             {{-- <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:48%"> --}}
                             {{-- <button type="submit" class="btn btn-dark">Pilih</button> --}}
-                            <input type="image" class="mySubmit" src="{{ asset('assets/images')}}/{{ $packages->package_image }}" name="submit" style="width:48%" alt="submit"/>
                         @endif
                         @endforeach
 
-                        <div class="pricing px-3">
+                        <div class="pricing">
                             <div class="row">
                               @foreach ($package as $packages)
-                              <div class="col-lg-4">
-                                <div class="cards mb-5 mb-lg-0">
-                                  <div class="cards-body py-4 px-4">
-                                    <h5 class="cards-title text-uppercase text-center">{{ $packages->name  }}</h5>
-                                    <h6 class="cards-price text-center">RM {{ $packages->price }}</h6>
-                                    <a href="{{ url('buypackage') }}/{{ $product->product_id }}/{{ $packages->package_id }}" class="btn btn-dark btn-block text-uppercase">Beli</a>
-                                  </div>
+                                <div class="mySubmit">
+                                    <input type="image" src="{{ asset('assets/images')}}/{{ $packages->package_image }}" name="submit" style="width:48%" alt="submit"/>
                                 </div>
-                                <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width: 100%">
-                                <a href="{{ url('buypackage') }}/{{ $product->product_id }}/{{ $packages->package_id }}" class="btn btn-warning btn-block text-uppercase">Beli</a>
                               </div>      
                               @endforeach
                             </div>
