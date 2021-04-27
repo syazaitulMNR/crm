0f62513a3d74ceaba17622906c75244cb243a871
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-27 11:42:28 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -28,6 +28,7 @@ Upgrade Pakej
                         @if($current_package->price >= $packages->price)
                         @else
                             <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:48%">
+                            <button type="submit" class="btn btn-dark">Pilih</a>
                         @endif
                         @endforeach
                     </div>
