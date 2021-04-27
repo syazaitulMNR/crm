950567e658252dad21c153d188056bd73cd91711
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-27 10:22:29 +0800

    Update UpgradeController.php

--- app/Http/Controllers/UpgradeController.php
+++ app/Http/Controllers/UpgradeController.php
@@ -10,12 +10,12 @@ use App\Student;
 
 class UpgradeController extends Controller
 {
-    public function choose_package($product_id, $package_id, $payment_id, $stud_id, Request $request){
+    public function choose_package($product_id, $package_id, $stud_id, $payment_id, Request $request){
 
         $product = Product::where('product_id', $product_id)->first();
         $package = Package::where('package_id', $package_id)->first();
-        $payment = Payment::where('payment_id', $payment_id)->first();
         $student = Student::where('stud_id', $stud_id)->first();
+        $payment = Payment::where('payment_id', $payment_id)->first();
 
         dd($payment_id);
         // return view('upgrade.choose_package', compact('product', 'package', 'payment', 'student'));
