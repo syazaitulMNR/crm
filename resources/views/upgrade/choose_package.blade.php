7a5e8eda11362a35dbed20ebb5e264cd3efc355c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 15:49:20 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -98,8 +98,8 @@ Upgrade Pakej
 </div>
 
 <script>
-    var x = 5;
-    var y = 2;
+    var x = '{{ $current_package->price }}';
+    var y = 1;
     var z = x + y;
     document.getElementById("price").innerHTML = z;
     </script>
