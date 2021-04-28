3872759632bdf7ff0db77504f91516951e25d8c7
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 10:43:11 +0800

    update

--- resources/views/admin/addpackage.blade.php
+++ resources/views/admin/addpackage.blade.php
@@ -25,7 +25,7 @@
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         
         <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-            <a href="dashboard">Dashboard</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Create Package</b>
+            <a href="{{ url('package')}}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Create Package</b>
         </div>
 
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
