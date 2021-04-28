45f2ede6c5fffdf00aaefecf004e949f2243c1c4
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 11:44:15 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -56,7 +56,7 @@ Upgrade Pakej
                     <div class="row">
                         <div class="col-lg-4 col-md-6 col-sm-10 pb-4 d-block m-auto">
                             <div class="pricing-item py-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29);">
-                                <img src="{{ asset('assets/images')}}/{{ $current_package->package_image }}" style="width:60%">
+                                <img src="{{ asset('assets/images')}}/{{ $current_package->package_image }}" style="width:70%">
                                 {{-- <div class="pt-4 pb-2" style="letter-spacing: 2px">
                                     <h4>{{ $current_package->name }}</h4>
                                 </div>
@@ -87,7 +87,7 @@ Upgrade Pakej
                         @else
                         <div class="col-lg-4 col-md-6 col-sm-10 pb-4 d-block m-auto">
                             <div class="pricing-item py-4">
-                                <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:60%">
+                                <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:70%">
                                 {{-- <div class="pt-4 pb-2" style="letter-spacing: 2px">
                                     <h4>{{ $packages->name }}</h4>
                                 </div>
