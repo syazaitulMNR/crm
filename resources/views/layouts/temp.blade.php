212e46ab528c53e2f1fa2e2f998917810ad5540f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 11:59:15 +0800

    Update temp.blade.php

--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -26,8 +26,7 @@
         <script src="https://js.stripe.com/v3/"></script>
 
         <!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">		
-		<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         
         <!-- Styles -->
@@ -57,7 +56,7 @@
             }
 
             body {
-                /* font-family: Helvetica, arial, verdana; */
+                font-family: "baloo";
                 background: transparent;
             }
 
