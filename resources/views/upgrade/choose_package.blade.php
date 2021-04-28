af0ad26578ca7bc526b3be14452914417d25cc08
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 12:02:35 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -35,8 +35,8 @@ Upgrade Pakej
                     <div class="row">
                         <div class="col-lg-4 col-md-6 col-sm-10 pb-4 d-block m-auto">
                             <div class="pricing-item py-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29);">
-                                <img src="{{ asset('assets/images')}}/{{ $current_package->package_image }}" style="width:70%">
-                                {{-- <div class="pt-4 pb-2" style="letter-spacing: 2px">
+                                {{-- <img src="{{ asset('assets/images')}}/{{ $current_package->package_image }}" style="width:70%"> --}}
+                                <div class="pt-4 pb-2" style="letter-spacing: 2px">
                                     <h4>{{ $current_package->name }}</h4>
                                 </div>
                                 <div class="text-primary" style="font-size: 75px">
@@ -55,7 +55,7 @@ Upgrade Pakej
                                     <h1 style="font-weight: 1000; font-size: 3.5em;">
                                         <span style="font-size: 20px;">RM</span>{{ $current_package->price }}
                                     </h1>
-                                </div> --}}
+                                </div>
                                 <div class="pricing-button pt-4 pb-1">
                                     <button type="button" class="btn btn-outline-danger w-75" disabled>Pakej Semasa</button>
                                 </div>
@@ -66,8 +66,8 @@ Upgrade Pakej
                         @else
                         <div class="col-lg-4 col-md-6 col-sm-10 pb-4 d-block m-auto">
                             <div class="pricing-item py-4">
-                                <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:70%">
-                                {{-- <div class="pt-4 pb-2" style="letter-spacing: 2px">
+                                {{-- <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:70%"> --}}
+                                <div class="pt-4 pb-2" style="letter-spacing: 2px">
                                     <h4>{{ $packages->name }}</h4>
                                 </div>
                                 <div class="text-primary" style="font-size: 75px">
@@ -86,7 +86,7 @@ Upgrade Pakej
                                     <h1 style="font-weight: 1000; font-size: 3.5em;">
                                         <span style="font-size: 20px;">RM</span>{{ $packages->price }}
                                     </h1>
-                                </div> --}}
+                                </div>
                                 <div class="pricing-button pt-4 pb-1">
                                     <button type="button" class="btn btn-danger w-75">Tukar Pakej</button>
                                 </div>
