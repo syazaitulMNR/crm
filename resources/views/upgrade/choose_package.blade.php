14fed8217c3c563036d348baac8f8770228c0e83
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 15:21:18 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -44,7 +44,7 @@ Upgrade Pakej
                                 </div>
                                 <div class="pricing-price pb-1 text-dark color-dark-text mb-3">
                                     <h1 style="font-weight: 1000; font-size: 3.5em;">
-                                        <span style="font-size: 20px;">RM</span><p class="text-decoration-line-through">{{ $current_package->price }}</p>
+                                        <span style="font-size: 20px;">RM</span><span class="text-decoration-line-through">{{ $current_package->price }}</span>
                                     </h1>
                                 </div>
                                 <div class="pricing-button pt-1 pb-1">
