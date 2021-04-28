6a168754a517a5eb96aa53ddf3951c08be618bea
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 14:58:43 +0800

    Update pay_upgrade.blade.php

--- resources/views/upgrade/pay_upgrade.blade.php
+++ resources/views/upgrade/pay_upgrade.blade.php
@@ -1,68 +1,61 @@
 @extends('layouts.temp')
 
 @section('title')
-Pendaftaran Pembeli
+Upgrade Pakej
 @endsection
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 px-3 py-5 text-center">
-            <img src="/assets/images/logo.png" style="max-width:200px">
+        <div class="col-md-12 px-3 pt-5 pb-3 text-center border-bottom">
+            <img src="/assets/images/logo.png" style="max-width:150px">
             <h1 class="text-dark px-4 pt-3">{{ $product->name }}</h1>
         </div>
 
-        <div class="col-md-12 d-flex justify-content-center pb-5">
+        <div class="col-md-12 py-3">
             <form action="" method="POST">
                 @csrf
-                <div class="pricing-item bg-white py-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29); border-radius: 5px;">  
-                      
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul class="px-3">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
+  
+                {{-- <div class="w-50 px-3 py-3 pt-md-4 pb-md-4 mx-auto text-center">
+                    <h1 class="font-weight-bold">{{ $product->name }}</h1>
+                    <p class="lead">Hai! Sila buat pilihan di bawah untuk upgrade pakej.</p>
+                </div> --}}
+                <div class="container text-center">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-10 pb-4 d-block mx-auto">
+                            <div class="pricing-item bg-white py-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29); border-radius: 5px;">
+                                <div class="pb-2" style="letter-spacing: 2px">
+                                    <h4>{{ $current_package->name }}</h4>
+                                </div>
+                                <div class="text-success" style="font-size: 75px">
+                                    <i class="fa fa-check-circle"></i>
+                                </div>
+                                <div class="pricing-description">
+                                    <ul class="list-unstyled mt-3 mb-1 py-2 px-1">
+                                        @foreach($feature as $features)
+                                        @if($current_package->package_id == $features->package_id)
+                                            <li class="text-break">{{ $features->name }}</li>
+                                            <hr class="my-1 mx-3">
+                                        @endif
                                         @endforeach
                                     </ul>
                                 </div>
-                            @endif
-  
-                        <div class="form-group row">
-                            {{-- <input type="hidden" value="{{ $payment_id ?? ''}}" class="form-control" name="payment_id" readonly/> --}}
-                            <input type="hidden" value="{{ $product->product_id }}" class="form-control" name="product_id" readonly/>
-                            <input type="hidden" value="{{ $current_package->package_id }}" class="form-control" name="package_id" readonly/>
-                            <input type="hidden" value="{{ $student->stud_id }}" class="form-control" name="stud_id" readonly/>
-
-                            <div class="col-md-6 pb-2">
-                                <label for="package">Pakej:</label>
-                                <input type="text"  value="{{ $current_package->name }}" class="form-control" readonly/>
-                            </div>
-                            {{-- <div class="col-md-6 pb-2">
-                                <label for="price">Harga:</label>
-                                <input type="text" value="{{ $current_package->price }}" class="form-control" readonly/>
-                                <input type="hidden" id="price" name="price" value="{{ $current_package->price }}" disabled>
-                            </div> --}}
-                            
-                            <div class="col-md-6 pb-2">
-                                <label for="totalprice">Jumlah Harga (RM)</label><br>
-                                <h3><input type="text" id="totalprice" name="totalprice" value="" style="border: none; outline-width: 0;" readonly></h3>
+                                <div class="pricing-price pb-1 text-dark color-dark-text mb-3">
+                                    <h1 style="font-weight: 1000; font-size: 3.5em;">
+                                        <span style="font-size: 20px;">RM</span>{{ $current_package->price }}
+                                    </h1>
+                                </div>
+                                <div class="pricing-button pt-1 pb-1">
+                                    <button type="button" class="btn btn-lg btn-outline-dark w-75" disabled>Pakej Semasa</button>
+                                </div>
                             </div>
                         </div>
-  
-                    
-                        <div class="col-md-12">
-                            <div class="pull-left">
-                                <a href="" class="btn btn-danger">Kembali</a>
-                            </div>
-                            <div class="pull-right">
-                                <button type="submit" class="btn btn-dark">Seterusnya</button>
-                            </div>
-                        </div>
+                    </div>
                 </div>
+                
+                    
             </form>
         </div>
-
     </div>
 </div>
-
 @endsection
