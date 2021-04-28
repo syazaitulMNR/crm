a5ca1e8aaddada64c2152c3798e1c51e6130a143
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-28 09:46:06 +0800

    Update choose_package.blade.php

--- resources/views/upgrade/choose_package.blade.php
+++ resources/views/upgrade/choose_package.blade.php
@@ -27,7 +27,7 @@ Upgrade Pakej
                         @foreach($package as $packages)
                         @if($current_package->price >= $packages->price)
                         @else
-                            <img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:48%">
+                            <button><img src="{{ asset('assets/images')}}/{{ $packages->package_image }}" style="width:48%"></button>
                         @endif
                         @endforeach
                     </div>
