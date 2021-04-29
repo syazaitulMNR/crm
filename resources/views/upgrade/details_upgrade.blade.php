42c8a7811478978fc1d01f4c8a572b231d4a9ea6
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 10:52:35 +0800

    Update details_upgrade.blade.php

--- resources/views/upgrade/details_upgrade.blade.php
+++ resources/views/upgrade/details_upgrade.blade.php
@@ -50,7 +50,7 @@ Upgrade Pakej
                     <div class="row">
                         <div class="col-auto pb-4 d-block mx-auto">
                             <div class="pricing-item bg-white py-4 px-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29); border-radius: 5px;">
-                                <div class="alert alert-primary text-left" role="alert">
+                                <div class="alert alert-info text-left" role="alert">
                                     <i class="fas fa-info-circle"></i> <span class="border-left">Anda akan dinaik taraf kepada pakej {{ $package->name }}</span> 
                                   </div>
                                 <div class="table-responsive">
