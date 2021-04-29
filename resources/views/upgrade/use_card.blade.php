41e3f3d6a77346c8ce1cd7dfa79f508cece866d4
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 11:46:21 +0800

    Update use_card.blade.php

--- resources/views/upgrade/use_card.blade.php
+++ resources/views/upgrade/use_card.blade.php
@@ -122,10 +122,10 @@ Upgrade Pakej
                     
                                 
                                 <div class="col-md-12 pb-5">
-                                    <div class="shadow-sm p-3 mb-5 bg-body rounded pull-left">
+                                    <div class="pull-left">
                                         <a href="{{ url('pay-upgrade') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->stud_id }}" class="btn btn-circle btn-lg btn-outline-dark"><i class="fas fa-arrow-left py-1"></i></a>
                                     </div>
-                                    <div class="shadow-sm p-3 mb-5 bg-body rounded pull-right">
+                                    <div class="pull-right">
                                         <button type="submit" class="btn btn-circle btn-lg btn-success"><i class="fas fa-check py-1"></i></button>
                                     </div>
                                 </div>
