6108e971c0d1f1370d9a4aac71e6e29f864264a4
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 09:42:54 +0800

    Update pay_upgrade.blade.php

--- resources/views/upgrade/pay_upgrade.blade.php
+++ resources/views/upgrade/pay_upgrade.blade.php
@@ -96,15 +96,12 @@ Upgrade Pakej
                                     <span id="price"></span>
                                 </div> --}}
                                 <div class="row-fluid">
-                                    <div class="col-md-12">
-                                        <div class="pricing-button pt-1 pb-1 text-left">
-                                            <button type="button" class="btn btn-outline-dark rounded-circle"><i class="fas fa-arrow-left py-1"></i></button>
-                                        </div>
-                                        <div class="pricing-button pt-1 pb-1 text-right">
-                                            <button type="button" class="btn btn-dark rounded-circle"><i class="fas fa-arrow-right py-1"></i></button>
-                                        </div>
+                                    <div class="pricing-button pt-1 pb-1 text-left">
+                                        <button type="button" class="btn btn-outline-dark rounded-circle"><i class="fas fa-arrow-left py-1"></i></button>
                                     </div>
-                                    
+                                    <div class="pricing-button pt-1 pb-1 text-right">
+                                        <button type="button" class="btn btn-dark rounded-circle"><i class="fas fa-arrow-right py-1"></i></button>
+                                    </div>                                    
                                 </div>
                             </div>
                         </div>
