17d49ce2ec4492936eca8a4790d31724057acde9
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 09:57:44 +0800

    Update pay_upgrade.blade.php

--- resources/views/upgrade/pay_upgrade.blade.php
+++ resources/views/upgrade/pay_upgrade.blade.php
@@ -95,14 +95,29 @@ Upgrade Pakej
                                     <p style="text-decoration: line-through;">RM{{ $current_package->price }}</p>
                                     <span id="price"></span>
                                 </div> --}}
-                                <div class="col-md-12 pb-5">
+
+                                <div class="form-group row">
+                                    <div class="col-md-12 px-5">
+                                        <button type="submit" class="button button4" name="pay_method" value="{{ $stripe ?? '' }}">
+                                            <i class="far fa-credit-card fa-3x"></i>
+                                            <br>Kad Debit/Kredit
+                                        </button>
+                                    
+                                        <button type="submit" class="button button4" name="pay_method" value="{{ $billplz ?? '' }}">
+                                            <i class="fas fa-university fa-3x"></i>
+                                            <br>FPX
+                                        </button>
+                                    </div>
+                                </div>
+
+                                {{-- <div class="col-md-12 pb-5">
                                     <div class="pull-left">
                                         <a href="{{ url('upgrade-package') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->stud_id }}" class="btn btn-outline-dark rounded-circle"><i class="fas fa-arrow-left py-1"></i></a>
                                     </div>
                                     <div class="pull-right">
                                         <button type="submit" class="btn btn-dark rounded-circle"><i class="fas fa-arrow-right py-1"></i></button>
                                     </div>
-                                </div>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
