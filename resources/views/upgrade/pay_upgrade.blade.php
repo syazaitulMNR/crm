3b608b2492ed5c80a33b4a17678e9a7d38621ddd
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 09:47:45 +0800

    Update pay_upgrade.blade.php

--- resources/views/upgrade/pay_upgrade.blade.php
+++ resources/views/upgrade/pay_upgrade.blade.php
@@ -95,14 +95,12 @@ Upgrade Pakej
                                     <p style="text-decoration: line-through;">RM{{ $current_package->price }}</p>
                                     <span id="price"></span>
                                 </div> --}}
-                                <div class="col-md-12">
                                     <div class="pull-left">
                                         <a href="{{ url('upgrade-package') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->stud_id }}" class="btn btn-outline-dark rounded-circle"><i class="fas fa-arrow-left py-1"></i></a>
                                     </div>
                                     <div class="pull-right">
                                         <button type="submit" class="btn btn-dark rounded-circle"><i class="fas fa-arrow-right py-1"></i></button>
                                     </div>
-                                </div>
                             </div>
                         </div>
                     </div>
