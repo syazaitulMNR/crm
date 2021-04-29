f927d34946375c31cf05a412270f0699d437ab58
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 10:45:06 +0800

    Update details_upgrade.blade.php

--- resources/views/upgrade/details_upgrade.blade.php
+++ resources/views/upgrade/details_upgrade.blade.php
@@ -83,7 +83,7 @@ Upgrade Pakej
                                                     </select>
                                                 </td>
                                                 <td class="text-center">
-                                                    RM <input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none;" readonly>
+                                                    RM <input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none; width: 50px" readonly>
                                                 </td>
                                             </tr>
                                         </tbody>
