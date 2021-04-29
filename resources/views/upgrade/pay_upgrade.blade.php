827ef0db85ee0b4016dfe01878c5dee76ede1cd2
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-04-29 09:07:40 +0800

    Update pay_upgrade.blade.php

--- resources/views/upgrade/pay_upgrade.blade.php
+++ resources/views/upgrade/pay_upgrade.blade.php
@@ -51,7 +51,7 @@ Upgrade Pakej
                         <div class="col-auto pb-4 d-block mx-auto">
                             <div class="pricing-item bg-white py-4 px-4" style=" box-shadow: 0px 0px 30px -7px rgba(0,0,0,0.29); border-radius: 5px;">
                                 <div class="pb-2" style="letter-spacing: 2px">
-                                    <h4>{{ $current_package->name }} <i class="fas fa-arrow-right  fa-2x"></i> New Package</h4>
+                                    <h4>{{ $current_package->name }} <i class="fas fa-arrow-right  fa-1x"></i> New Package</h4>
                                 </div>
 
                                 <table class="table table-light">
