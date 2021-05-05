433bd9e4e042c5555192e38b9ab4680877b2d4c1
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-05 16:15:54 +0800

    Update CertController.php

--- app/Http/Controllers/CertController.php
+++ app/Http/Controllers/CertController.php
@@ -41,21 +41,29 @@ class CertController extends Controller
         return view('certificate.show_info', compact('product', 'student'));
     }
 
-    public function get_cert($product_id, $stud_id){
+    public function get_cert($product_id, $stud_id)
+    {
         $product = Product::where('product_id', $product_id)->first();
         $student = Student::where('stud_id', $stud_id)->first();
-                
-        $data['name']=$student->first_name;
-        $data['ic']=$student->ic;
 
-        $data['program_name']=$product->name;
-
-        $data['date_receive']=date('d-m-Y');
-        $data['product_id']=$product_id;        
-        $data['student_id']=$stud_id;
-
-        $pdf = PDF::loadView('certificate.e-cert', $data);
-        return $pdf->download( $product->name . '.pdf');
+        return view('certificate.e-cert', compact('product', 'student'));
     }
+
+    // public function get_cert($product_id, $stud_id){
+    //     $product = Product::where('product_id', $product_id)->first();
+    //     $student = Student::where('stud_id', $stud_id)->first();
+                
+    //     $data['name']=$student->first_name;
+    //     $data['ic']=$student->ic;
+
+    //     $data['program_name']=$product->name;
+
+    //     $data['date_receive']=date('d-m-Y');
+    //     $data['product_id']=$product_id;        
+    //     $data['student_id']=$stud_id;
+
+    //     $pdf = PDF::loadView('certificate.e-cert', $data);
+    //     return $pdf->download( $product->name . '.pdf');
+    // }
 
 }
