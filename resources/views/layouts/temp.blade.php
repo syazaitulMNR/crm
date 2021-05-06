2e3883c578d7a2809c8608e2603f0d4e73c639d3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-06 14:48:54 +0800

    Update temp.blade.php

--- resources/views/layouts/temp.blade.php
+++ resources/views/layouts/temp.blade.php
@@ -30,8 +30,7 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         
         <!-- Styles -->
-        {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         @yield('css')
 
         {{-- CDN Starts Font Awesome --}}
