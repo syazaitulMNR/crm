e3deaa2d6a15675978fef74e6af801f346149d72
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-06 15:28:18 +0800

    update

--- resources/views/certificate/e-cert.blade.php
+++ resources/views/certificate/e-cert.blade.php
@@ -36,7 +36,7 @@ E-Certificate
 
 <div class="row">
     <div class="col-md-12 pt-5">
-        <img src="/assets/images/certified.png" style="max-width:150px">
+        <img src="/assets/images/certified_cop.png" style="max-width:150px">
     </div>
 </div>
 @endsection
