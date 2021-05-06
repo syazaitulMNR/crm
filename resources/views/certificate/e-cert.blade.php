00b4c3f19505dfb25cbbd70f2d69139bcb6be611
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-06 15:36:03 +0800

    Update e-cert.blade.php

--- resources/views/certificate/e-cert.blade.php
+++ resources/views/certificate/e-cert.blade.php
@@ -35,7 +35,7 @@ E-Certificate
 </div>
 
 <div class="row">
-    <div class="col-md-12 pt-5 px-5">
+    <div class="col-md-12 py-5 px-5">
         <div style="padding-left: 20%; padding-right: 20%;">  
             <img src="/assets/images/certified_cop.png" style="max-width:150px;">
         </div>
