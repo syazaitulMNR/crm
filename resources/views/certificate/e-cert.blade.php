1bdb2743161a99df90db328b9e957b04258f91c0
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-06 16:19:02 +0800

    Update e-cert.blade.php

--- resources/views/certificate/e-cert.blade.php
+++ resources/views/certificate/e-cert.blade.php
@@ -43,7 +43,7 @@ E-Certificate
             <img src="/assets/images/certified_cop.png" style="max-width:150px;">
         </div>
         <div class="col-md-6 text-center" style="padding-right: 10%;">  
-            <img src="/assets/images/signature.png" style="max-width:100px;">
+            <img src="/assets/images/signature.png" style="max-width:80px;">
             <h3 class="border-top pt-2">Najib Asaddok</h3>
             <p class="lead m-0"> Chief Executive Officer (CEO)</p>
             <p class="lead m-0"> Momentum Internet Sdn Bhd </p>
