debd1b95c3bb30583cf680cc7e4c1e5384a5b30d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-07 12:27:45 +0800

    Update e-cert.blade.php

--- resources/views/certificate/e-cert.blade.php
+++ resources/views/certificate/e-cert.blade.php
@@ -31,21 +31,27 @@ E-Certificate
 <div class="bg">
     <div class="container" style="border-width:10px !important">
         <div class="py-5"></div>
-            <div class="row py-5">
-                <div class="col-md-12 pt-5 text-center">
-                    <img src="/assets/images/logo.png" style="max-width:200px">
-                </div>
+        <div class="row py-5">
+            <div class="col-md-12 pt-5 text-center">
+                <img src="/assets/images/logo.png" style="max-width:200px">
             </div>
+        </div>
         
+        <div class="row">
             <div class="col-md-12 py-4 text-center">
-                <h1 class="display-1 text-break">CERTIFICATE</h1>
-                <h1 class="display-4">OF COMPLETION</h1>
+                <h1>CERTIFICATE</h1>
+                <h2>OF COMPLETION</h2>
             </div>
+        </div>
+        
+        <div class="row">
             <div class="col-md-12 py-5 text-center">
                 <h3> {{ $student->first_name }} {{ $student->last_name }}</h3>
                 <h5> ({{ $student->ic }})</h5>
             </div>
-            
+        </div>
+        
+        <div class="row pb-5">
             <div class="col-md-12 py-5 text-center">
                 <p class="lead"> Successfully complete the</p>
                 <h3> {{ $product->name }}</h3>
