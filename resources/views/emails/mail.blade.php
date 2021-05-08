94d7822b06fbb9998aaad717c4e27b49558557ba
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-08 09:25:48 +0800

    Update mail.blade.php

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -15,41 +15,29 @@
                   <table style="border: none; cellpadding: 0; cellspacing: 0;" >
                     <tr>
                       <td>
-                        <h3>Pendaftaran {{ $product_name }} anda telah berjaya</h3>
+                        <h3>Pendaftaran anda telah berjaya!</h3>
                         <table style="border: none; cellpadding: 0; cellspacing: 0;" class="btn btn-primary">
                           <tbody>
                             <tr>
-                                <td>Assalamualaikum & Salam Sejahtera Tuan/Cik/Puan,<br><br>
-                                Terima Kasih kerana berminat untuk menyertai:</td>
+                                <td>Assalamualaikum & Salam Sejahtera Tuan/Puan,<br><br>
+                                Tahniah kerana sudah mendaftar ke Program {{ $product_name }}.
+                                Program ini akan berlangsung pada:</td>
                             </tr>
                             <tr>
                                 <td>
-                                  <b>Program</b> : {{ $product_name }}<br>
                                   <b>Tarikh</b> : {{ $date_from }} hingga {{ $date_to }}<br>
                                   <b>Waktu</b> : {{ $time_from }} hingga {{ $time_to}}<br>
                                   <b>Order ID</b> : {{ $payment_id }}
                                 </td>
-                                {{-- <td class="align-center">
-                                    <table style="padding-left:35%; padding-right:30%">
-                                      <tbody>
-                                        <tr>
-                                          <td style="width: 24%;"><p class="align-left"><b>Pakej</b></p></td>
-                                          <td style="width: 1%;"><p class="align-left"><b>:</b></p></td>
-                                          <td style="width: 25%;"><p class="align-left">{{ $package }}</p></td>
-                                        </tr>
-                                        <tr>
-                                          <td style="width: 24%;"><p class="align-left"><b>Kuantiti</b></p></td>
-                                          <td style="width: 1%;"><p class="align-left"><b>:</b></p></td>
-                                          <td style="width: 25%;"><p class="align-left">{{ $quantity }}</p></td>
-                                        </tr>
-                                        <tr>
-                                          <td style="width: 24%;"><p class="align-left"><b>Jumlah</b></p></td>
-                                          <td style="width: 1%;"><p class="align-left"><b>:</b></p></td>
-                                          <td style="width: 25%;"><p class="align-left">RM {{ $total }}</p></td>
-                                        </tr>
-                                      </tbody>
-                                    </table>
-                                  </td> --}}
+                            </tr>
+                            <hr>
+                            <tr>
+                              <td>
+                                <p>PENTING! Wajib follow step di bawah, supaya anda dapat info kelas dan update terkini.</p>
+                                <br>
+                                <p>Jawab Soal Selidik ini —> <a href="http://google.com"> {{ $product_name }}</a></p>
+                                <p>Selepas jawab Soal Selidik, anda akan dibawa ke Channel Telegram Khas. Segala update kelas dan info program akan diupdate di Telegram Channel tersebut.</p>
+                              </td>
                             </tr>
                             <hr>
                             <tr>
@@ -76,7 +64,7 @@
                             </tr>
                             <hr>
                             <tr>
-                              <td>
+                              <td class="align-center">
                                 <p>Jika anda berminat untuk naik taraf pakej, sila klik butang di bawah.</p>
                               </td>
                             </tr>
@@ -87,14 +75,17 @@
                                     <tr>
                                     <td> 
                                       <a class="btn btn-primary py-3 px-4" href="{{ url('upgrade-package') }}/{{ $productId }}/{{ $packageId }}/{{ $student_id }}/{{ $payment_id }}">Upgrade Package</a>
-                                      <br>
-                                      <p>*Naik taraf pakej hanya dibenarkan sebelum melakukan pengemaskinian maklumat peserta.</p>
                                       {{-- <a href="https://order.momentuminternet.com/product/up-gen-storm-2021-24-25-apr-2021-online-flex/" class="btn btn-primary py-3 px-4">Flex</a>
                                       <a href="https://order.momentuminternet.com/product/up-gen-storm-2021-24-25-apr-2021-online-vip/" class="btn btn-primary py-3 px-4">VIP</a> --}}
                                     </td>
                                     </tr>
                                   </tbody>
                                 </table>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <p style="font-size: 10px; color:red">*Naik taraf pakej hanya dibenarkan sebelum melakukan pengemaskinian maklumat peserta.</p>
                               </td>
                             </tr>
                           </tbody>
@@ -128,12 +119,6 @@
             <!-- START FOOTER -->
             <div class="footer">
               <table style="border: none; cellpadding: 0; cellspacing: 0;" >
-                {{-- <tr>
-                  <td class="content-block">
-                    <span class="apple-link">Coloured.com.ng | Feminism | Culture | Law | Feminists Rising</span>
-                    <br> Don't like these emails? <a href="#">Unsubscribe</a>.
-                  </td>
-                </tr> --}}
                 <tr>
                   <td class="content-block powered-by">
                     MOMENTUM INTERNET (1079998-A) © 2020 ALL RIGHTS RESERVED​
