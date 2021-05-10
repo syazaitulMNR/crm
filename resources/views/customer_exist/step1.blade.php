f2d0667d1f6933c46f71099f3460a86fe4ab143a
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-10 13:08:13 +0800

    Update step1.blade.php

--- resources/views/customer_exist/step1.blade.php
+++ resources/views/customer_exist/step1.blade.php
@@ -83,7 +83,7 @@ Pendaftaran Pembeli
                             </div>
                         </div> --}}
 
-                        <div class="col-md-12">
+                        <div class="col-md-12 pb-3">
                             <div class="pull-right">
                                 <button type="submit" class="btn btn-circle btn-lg btn-dark"><i class="fas fa-arrow-right"></i></button>
                             </div>
