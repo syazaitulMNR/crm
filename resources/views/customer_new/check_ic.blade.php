eb447e731e3601461e033f98e5bfa024b52d9d83
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-10 12:18:39 +0800

    Update check_ic.blade.php

--- resources/views/customer_new/check_ic.blade.php
+++ resources/views/customer_new/check_ic.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-{{-- <div class="row">
+<div class="row">
     <div class="col-md-12 px-2 py-5 text-center">
         <img src="/assets/images/logo.png" style="max-width:200px">
         <h1 class="text-dark px-4 pt-3">{{ $product->name }}</h1>
@@ -17,18 +17,22 @@
             <form action="{{ url('verification') }}/{{ $product->product_id }}/{{ $package->package_id }}" method="get">
                 @csrf
                 <div class="col-md-12">
-                    <input type="text" class="form-control" name="ic" placeholder="tanpa '-'" maxlength="12" required="" >
-                    <p style="font-size: 10pt; color:#202020; text-align: left;"><em>Cth: 91042409**** / A********</em></p>
+                    <input type="text" class="form-control" name="ic" placeholder="tanpa '-' .Cth: 91042409**** / A********" maxlength="12" required="" >
                 </div>
-                <div class="col-md-12 pb-3">
+                {{-- <div class="col-md-12 pb-3">
                     <button type="submit" class="text-white btn btn-block" style="background-color: #202020">Seterusnya</button>
+                </div> --}}
+                <div class="col-md-12 pb-5">
+                    <div class="pull-right">
+                        <button type="submit" class="btn btn-circle btn-lg btn-dark"><i class="fas fa-arrow-right"></i></button>
+                    </div>
                 </div>
             </form>
         </div>
     </div>
-</div> --}}
+</div>
 
-<div class="row">
+{{-- <div class="row">
     <div class="col-md-12 px-2 py-5 text-center">
         <img src="/assets/images/logo.png" style="max-width:150px">
         <h1 class="display-4 text-dark px-4 pt-3">{{ $product->name }}</h1>
@@ -58,7 +62,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 <footer class="text-center px-4 py-5">
     <b>Momentum Internet (1079998-A) © 2020 All Rights Reserved​</b>
 </footer>
