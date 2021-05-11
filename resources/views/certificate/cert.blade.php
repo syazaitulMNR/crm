54433ad96919a56a4d21595ccfeb752ef53025cf
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-11 16:20:16 +0800

    Update cert.blade.php

--- resources/views/certificate/cert.blade.php
+++ resources/views/certificate/cert.blade.php
@@ -17,7 +17,7 @@
     <table role="presentation" border="1" cellpadding="0" cellspacing="0" width="100%">
         <tr>
             <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0;">
-                <img src="/assets/images/top.png" alt="Top" width="300" height="230" style="display: block;" />
+                <img src="/assets/images/top.png" style="display: block;" />
             </td>
         </tr>
         <tr>
