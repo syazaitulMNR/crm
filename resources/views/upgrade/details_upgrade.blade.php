3ce49eb071b22d605503eca87f34a535061a45d6
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-11 11:43:16 +0800

    Update details_upgrade.blade.php

--- resources/views/upgrade/details_upgrade.blade.php
+++ resources/views/upgrade/details_upgrade.blade.php
@@ -49,7 +49,7 @@ Upgrade Pakej
 
                         @foreach ($package as $packages)
                         @if ($new_package->package_id == $packages->package_id)
-                        <table class="table table-borderless" style="width: 100px;">
+                        <table class="table table-borderless" style="width: 300px;">
                             <tr>
                                 <td >Pakej</td>
                                 <td>:</td>
