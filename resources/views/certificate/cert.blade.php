eacbebc9458a9e3deb4ff9d0ceb9e6d821baa017
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-17 17:05:52 +0800

    Update cert.blade.php

--- resources/views/certificate/cert.blade.php
+++ resources/views/certificate/cert.blade.php
@@ -12,7 +12,7 @@
         }
         .bg {
             /* The image used */
-            background-image: url("{{ asset('assets/images/certificate') }}/{{ $cert_image }}");
+            background-image: url('{{ asset('assets/images/certificate') }}/{{ $cert_image }}');
 
             /* Full height */
             height: 100%;
