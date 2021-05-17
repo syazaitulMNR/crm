714f429844ad62ef4823ebd8751fc34af9cdd674
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-17 11:35:46 +0800

    update

--- resources/views/certificate/cert.blade.php
+++ resources/views/certificate/cert.blade.php
@@ -62,11 +62,11 @@
             </td>
         </tr>
         <tr>
-            <td align="center" style="padding: 80px 0 50px 0;">
-                <img src="https://i.ibb.co/5k9QRyk/certified-cop.png" style="max-width:150px; margin: 0;">
+            <td align="center" style="padding: 80px 0 100px 0;">
+                <img src="{{ asset('assets/images/cert_logo.png') }}" style="max-width:150px; margin: 0;">
             </td>
-            <td align="center" style="padding: 80px 0 50px 0;">
-                <img src="https://i.ibb.co/7WDt18B/sign.png" style="max-width:80px; margin: 0;">
+            <td align="center" style="padding: 80px 0 100px 0;">
+                <img src="{{ asset('assets/images/sign.png') }}" style="max-width:80px; margin: 0;">
                 <h3 style="margin: 0;">Najib Asaddok</h3>
                 <p style="margin: 0;"> Chief Executive Officer (CEO)</p>
                 <p style="margin: 0;"> Momentum Internet Sdn Bhd </p>
