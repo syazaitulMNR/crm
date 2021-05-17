aa7012007e1b3f76f4d504d18118497ca00ef11a
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-17 15:06:15 +0800

    Update check_detail.blade.php

--- resources/views/certificate/check_detail.blade.php
+++ resources/views/certificate/check_detail.blade.php
@@ -7,40 +7,45 @@ E-Certificate
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 px-3 pt-5 pb-3 text-center">
-            <img src="/assets/images/logo.png" style="max-width:150px">
+        <div class="col-md-12 px-3 py-5 text-center">
+            <img src="/assets/images/logo.png" style="max-width:200px">
             <h1 class="display-4 text-dark px-4 pt-3">{{ $product->name }}</h1>
+            <h6>Hai! Terima kasih kerana masih setia menghadiri program kami. Sila semak butiran peribadi anda.</h6>
         </div>
 
-        <div class="col-md-12 d-flex justify-content-center pb-5">
-  
+        <div class="col-md-12 d-flex justify-content-center pb-5">  
             <div class="card w-100 shadow">
-                <div class="card-header bg-dark text-white">Sila semak maklumat anda</div>
+                <div class="card-header bg-dark text-white">Maklumat Peserta</div>
 
-                <div class="card-body px-3">
+                <div class="card-body">
 
-                    <table class="table table-borderless">
-                        <tr>
-                            <td >No. IC</td>
-                            <td>:</td>
-                            <td class="text-break">{{ $student->ic }}</td>
-                        </tr>
-                        <tr>
-                            <td >Nama</td>
-                            <td>:</td>
-                            <td class="text-break">{{ $student->first_name }} {{ $student->last_name }}</td>
-                        </tr>
-                        <tr>
-                            <td >No Tel</td>
-                            <td>:</td>
-                            <td> {{ $student->phoneno }} </td>
-                        </tr>
-                        <tr>
-                            <td >Email</td>
-                            <td>:</td>
-                            <td> {{ $student->email }} </td>
-                        </tr>
-                    </table> 
+                    <div class="form-group row">
+
+                        <div class="col-md-12 pb-2">
+                            <label for="description">No. Kad Pengenalan/Passport:</label>
+                            <input type="text"  value="{{ $student->ic }}" class="form-control" readonly/>
+                        </div>
+
+                        <div class="col-md-6 pb-2">
+                            <label for="title">Nama Pertama:</label>
+                            <input type="text" value="{{ $student->first_name }}" class="form-control" readonly/>
+                        </div>
+                        <div class="col-md-6 pb-2">
+                            <label for="title">Nama Akhir:</label>
+                            <input type="text" value="{{ $student->last_name }}" class="form-control" readonly/>
+                        </div>
+
+                        <div class="col-md-6 pb-2">
+                            <label for="description">Emel:</label>
+                            <input type="text"  value="{{ $student->email }}" class="form-control" readonly/>
+                        </div>
+                        
+                        <div class="col-md-6 pb-2">
+                            <label for="description">No. Telefon:</label><br>
+                            <input type="tel" name="phoneno" value="{{ $student->phoneno }}" class="form-control" readonly/>
+                        </div>
+                    </div>
+                        
                 </div>
 
                 <div class="card-footer">
@@ -48,16 +53,29 @@ E-Certificate
                         <div class="pull-left">
                             <a href="{{ url('e-cert') }}/{{ $product->product_id }}" class="btn btn-circle btn-lg btn-outline-dark"><i class="fas fa-arrow-left" style="padding-top:35%"></i></a>
                         </div>
+                    </div>
+
+                    <div class="col-md-12 pb-3">
                         <div class="pull-right">
-                            <a href="{{ url('certificate') }}/{{ $product->product_id }}/{{ $student->stud_id }}" class="btn btn-circle btn-lg btn-success"><i class="fas fa-check py-1"></i></a>
+                            <button type="submit" name="myButton" class="btn btn-circle btn-lg btn-success"><i class="fas fa-check py-1"></i></button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    
     </div>
 </div>
 
+{{-- Disabled multiple submission on payment --------------------------------------------------------------------------------}}
+<script type="text/javascript">
 
+    function checkForm(form) // Submit button clicked
+    {
+        form.myButton.disabled = true;
+        form.myButton.value = "Wait...";
+        return true;
+    }
+
+</script>
+{{-- End Disabled multiple submission on payment ----------------------------------------------------------------------------}}
 @endsection
