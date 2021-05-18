f3728b5d6935fc220c5384165a75f0555a9f04ba
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-18 14:21:22 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light sticky-top px-5" style="background-color: #ffffff; padding-top:1%; padding-bottom:1%; box-shadow: 0 0px 30px 0 rgba(0, 0, 0, 0.2);">
     <a class="site-logo-inner" href="#">
-        <img src="https://momentuminternet.com/wp-content/uploads/2020/07/cropped-logo-momentum-4.jpg" class="custom-logo" alt="Momentum Internet &#8211;" srcset="https://momentuminternet.com/wp-content/uploads/2020/07/cropped-logo-momentum-4.jpg 1247w, https://momentuminternet.com/wp-content/uploads/2020/07/cropped-logo-momentum-4-300x223.jpg 300w, https://momentuminternet.com/wp-content/uploads/2020/07/cropped-logo-momentum-4-1024x761.jpg 1024w, https://momentuminternet.com/wp-content/uploads/2020/07/cropped-logo-momentum-4-768x571.jpg 768w" width="50" height="40" alt="">
+        <img src="{{ asset('assets/images/logo.png') }}" class="custom-logo" width="50" height="40" alt="">
     </a>
     
     <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left:2%;">
