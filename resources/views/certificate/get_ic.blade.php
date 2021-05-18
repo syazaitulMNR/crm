b68462fa8957432521348600513dcc82c03bed8f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-18 10:46:17 +0800

    Update get_ic.blade.php

--- resources/views/certificate/get_ic.blade.php
+++ resources/views/certificate/get_ic.blade.php
@@ -7,7 +7,7 @@ E-Certificate
 @section('content')
 <div class="row">
     <div class="col-md-12 px-3 pt-5 pb-3 text-center">
-        <img src="{{ asset('assets/images/certificate') }}/{{ $product->cert_image }}" style="max-width:150px">
+        <img src="{{ asset('assets/images/logo.png') }}" style="max-width:150px">
         <h1 class="display-4 text-dark px-4 pt-3">{{ $product->name }}</h1>
     </div>
 
