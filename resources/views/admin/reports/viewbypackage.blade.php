73629ad3bb02b31df016515db1a2ced7dc6d52ee
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-20 11:11:18 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -64,26 +64,26 @@
                 </div>
                 <form action="{{ url('new-customer/save') }}/{{ $product->product_id }}/{{ $package->package_id }}" name="form1" id="form1" method="POST"> 
                 @csrf
-                  <div class="form-group row">
-                      <label for="ic" class="col-form-label text-right">IC No. :</label>
+                  <div class="form-group row px-3">
+                      <label for="ic" class="col-sm-2 col-form-label text-right">IC No. :</label>
                       <div class="col-sm-10">
                       <input type="text" class="form-control" name="ic"  >
                       </div>
                   </div>
-                  <div class="form-group row">
-                      <label for="name" class="col-form-label text-right">Name :</label>
+                  <div class="form-group row px-3">
+                      <label for="name" class="col-sm-2 col-form-label text-right">Name :</label>
                       <div class="col-sm-10">
                       <input type="text" class="form-control" name="name" >
                       </div>
                   </div>
-                  <div class="form-group row">
-                      <label for="name" class="col-form-label text-right">Tel No. :</label>
+                  <div class="form-group row px-3">
+                      <label for="name" class="col-sm-2 col-form-label text-right">Tel No. :</label>
                       <div class="col-sm-10">
                       <input type="text" class="form-control" name="phoneno"  >
                       </div>
                   </div>
-                  <div class="form-group row">
-                      <label for="name" class="col-form-label text-right">Email :</label>
+                  <div class="form-group row px-3">
+                      <label for="name" class="col-sm-2 col-form-label text-right">Email :</label>
                       <div class="col-sm-10">
                       <input type="email" class="form-control" name="email"  >
                       </div>
