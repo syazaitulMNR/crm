2ce0324211e8e5141508838604eb7e70f4a24e1e
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-20 08:46:03 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -38,7 +38,7 @@
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-        <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p>
+        {{-- <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p> --}}
       </div>
 
       @if ($message = Session::get('updateprofile'))
