203bee02cf0fb70bab4618bb78b4b4d50cfbbe17
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-20 09:46:38 +0800

    Update select_event.blade.php

--- resources/views/admin/students/select_event.blade.php
+++ resources/views/admin/students/select_event.blade.php
@@ -18,7 +18,7 @@
    
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">New Customer</h1>
-        <p>Please select the event to continue</p>
+        <p>Select the event to continue</p>
     </div> 
 
     <div class="row">
