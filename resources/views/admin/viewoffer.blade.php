fe80a77d8a1011a6064abd69e8a475a8e53cffb5
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-21 15:24:13 +0800

    Update viewoffer.blade.php

--- resources/views/admin/viewoffer.blade.php
+++ resources/views/admin/viewoffer.blade.php
@@ -17,7 +17,7 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Manage Offer</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group mr-2">
+            <div class="mr-2">
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#newoffer">
                     <i class="fas fa-plus pr-1"></i> New Offer
