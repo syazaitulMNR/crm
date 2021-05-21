3d0b4402a8595cfcb178964282c590b7beb3211b
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-21 10:05:05 +0800

    Update trackprogram.blade.php

--- resources/views/admin/reports/trackprogram.blade.php
+++ resources/views/admin/reports/trackprogram.blade.php
@@ -12,11 +12,11 @@
   <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">   
      
     <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-      <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <b>Manage Customer</b>
+      <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <b>Customer</b>
     </div>
    
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Manage Customer</h1>
+        <h1 class="h2">Customer</h1>
     </div> 
 
     <div class="row">
