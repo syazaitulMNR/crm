e958b52b3737200b3a0403ab538cbd18f9e3ca8c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-23 23:15:38 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -239,7 +239,7 @@
             <div class="card bg-light py-4 mb-4 text-center shadow">
               <div class="card-block text-dark">
                 <div class="rotate">
-                <i class="fas fa-file-invoice-dollar fa-6x"></i>
+                <i class="fas fa-file-invoice-dollar fa-6x" style="color:rgba(0, 229, 255, 0.3)"></i>
                 </div>
                 <h3 class="pt-3 pl-3">{{$total}}</h3>
                 <h6 class="lead pb-1 pl-3">Total Purchased</h6>
@@ -248,7 +248,9 @@
 
             <div class="card bg-light py-4 mb-4 text-center shadow">
               <div class="card-block text-dark">
-                <i class="fas fa-check fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
+                <div class="rotate">
+                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
+                </div>
                 <h3 class="pt-3 pl-3">{{$totalsuccess}}</h3>
                 <h6 class="lead pb-1 pl-3">Paid</h6>
               </div>
@@ -256,7 +258,9 @@
 
             <div class="card bg-light py-4 mb-4 text-center shadow">
               <div class="card-block text-dark">
-                <i class="fas fa-times fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
+                <div class="rotate">
+                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
+                </div>
                 <h3 class="pt-3 pl-3">{{$totalcancel}}</h3>
                 <h6 class="lead pb-1 pl-3">Due</h6>
               </div>
