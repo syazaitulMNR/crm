b1c9f7d8e85c218188426dc64e9a8d258f3274cf
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-23 21:02:16 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -155,17 +155,51 @@
             <table class="table table-hover" id="successTable">
                 <thead>
                 <tr class="header">
-                    <th>#</th>
+                  <th>#</th>
+                  <th>IC No.</th>
+                  <th>Name</th>
+                  {{-- <th>Payment (RM)</th> --}} 
+                  <th>Status</th>
+                  <th>Purchase Date</th>
+                  <th><i class="fas fa-cogs"></i></th>
+                    {{-- <th>#</th>
                     <th>Order ID</th>
                     <th>Customer ID</th>
                     <th>Payment (RM)</th>
                     <th>Status</th>
                     <th>Purchase Date</th>
-                    <th><i class="fas fa-cogs"></i></th>
+                    <th><i class="fas fa-cogs"></i></th> --}}
                 </tr>
                 </thead>
                 <tbody>
-                @foreach ($payment as $key => $payments)    
+                  @foreach ($student as $students)    
+                  @foreach ($payment as $payments)
+                  @if ($payments->stud_id == $students->stud_id)
+                  @if ($product->product_id == $payments->product_id)  
+                  <tr>
+                      <td>{{ $students->stud_id }}</td>
+                      <td>{{ $students->ic }}</td>
+                      <td>{{ $students->first_name }}</td>
+                      {{-- <td>RM {{ $payments->totalprice }}</td> --}}
+                      <td>
+                        @if ($payments->status == 'paid')
+                          <span class="badge rounded-pill bg-success"> &nbsp;{{ $payments->status }}&nbsp; </span>
+                        @elseif ($payments->status == 'due')
+                          <span class="badge rounded-pill bg-danger"> &nbsp;{{ $payments->status }}&nbsp; </span>
+                        @else
+                          <p>NULL</p>
+                        @endif
+                      </td>
+                      <td>{{ date('d/m/Y', strtotime($payments->created_at)) }}</td>
+                      <td>
+                        <a class="btn btn-primary" href="{{ url('viewpayment') }}/{{ $product->product_id }}/{{ $payments->package_id }}/{{ $payments->payment_id }}/{{ $payments->stud_id }}"><i class="fas fa-edit"></i></a>
+                      </td>
+                  </tr>
+                  @endif
+                  @endif
+                  @endforeach
+                  @endforeach
+                {{-- @foreach ($payment as $key => $payments)    
                 @if ($product->product_id == $payments->product_id)  
                 <tr>
                     <td>{{ $payment->firstItem() + $key }}</td>
@@ -187,7 +221,7 @@
                     </td>
                 </tr>
                 @endif
-                @endforeach
+                @endforeach --}}
                 </tbody>
             </table>  
             @else
