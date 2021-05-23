13fc41d59783fbeb074fd1b613bbe082c17d76a8
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-23 23:13:43 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -249,7 +249,7 @@
             <div class="card bg-light py-4 mb-4 text-center shadow">
               <div class="card-block text-dark">
                 <div class="rotate">
-                  <i class="far fa-check fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
+                  <i class="fas fa-check fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
                 </div>
                 <h3 class="pt-3 pl-3">{{$totalsuccess}}</h3>
                 <h6 class="lead pb-1 pl-3">Paid</h6>
@@ -259,7 +259,7 @@
             <div class="card bg-light py-4 mb-4 text-center shadow">
               <div class="card-block text-dark">
                 <div class="rotate">
-                  <i class="far fa-times fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
+                  <i class="fas fa-times fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
                 </div>
                 <h3 class="pt-3 pl-3">{{$totalcancel}}</h3>
                 <h6 class="lead pb-1 pl-3">Due</h6>
