13d42476fdf713bf7350eeca31988106c333c2c6
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-24 08:50:49 +0800

    Update importexcel.blade.php

--- resources/views/admin/importexcel.blade.php
+++ resources/views/admin/importexcel.blade.php
@@ -122,7 +122,7 @@
 
             <div class="row float-right pt-3">
                 <div class="col-auto pt-1">
-                    <a class="btn btn-warning" href="{{ url('exportExcel') }}"><i class="fas fa-download pr-1"></i>Download the format</a>
+                    <a class="btn btn-warning" href="{{ url('exportExcel') }}"><i class="fas fa-download pr-2"></i>Download the format</a>
                 </div>
             </div>
 
