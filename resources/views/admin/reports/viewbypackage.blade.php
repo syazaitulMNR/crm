7bab9919f3542823956e81c95cc646924abf0cc3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-24 16:31:22 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -100,28 +100,28 @@
                     <div class="form-group row px-4">
                       <label for="ic" class="col-sm-4 col-form-label">Price (RM)</label>
                       <div class="col-sm-8">
-                      <input type="text" class="form-control" name="pay_price" id="price" value="{{ $package->price }}" readonly>   
+                      <input type="text" class="form-control" name="pay_price" id="price" value="{{ $package->price }}" required>   
                       </div>
                     </div>
                     <div class="form-group row px-4">
                         <label for="name" class="col-sm-4 col-form-label">Quantity</label>
                         <div class="col-sm-8">
-                          <input type="hidden" class="form-control" name="quantity" value="1" readonly>
+                          {{-- <input type="hidden" class="form-control" name="quantity" value="1" readonly> --}}
                           <label class="col-form-label">1</label>
-                          {{-- <select id="quantity" name="quantity" onchange="calculateAmount(this.value)" class="form-select" required>
+                          <select id="quantity" name="quantity" onchange="calculateAmount(this.value)" class="form-select" required>
                             <option value="" disabled selected>-- Tiket --</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
-                          </select> --}}
+                          </select>
                         </div>
                     </div>
                     <div class="form-group row px-4">
                         <label for="name" class="col-sm-4 col-form-label">Total Payment (RM)</label>
                         <div class="col-sm-8">
-                          <input type="hidden" class="form-control" name="totalprice" value="{{ $package->price }}" readonly> 
-                          <label class="col-form-label">{{$package->price}}</label>
-                        {{-- <input type="text" id="totalprice" class="form-control" name="totalprice" style="border: none; outline-width: 0; background-color: none;" readonly> --}}
+                          {{-- <input type="hidden" class="form-control" name="totalprice" value="{{ $package->price }}" readonly> 
+                          <label class="col-form-label">{{$package->price}}</label> --}}
+                          <input type="text" id="totalprice" class="form-control" name="totalprice" style="border: none; outline-width: 0; background-color: none;" readonly>
                         </div>
                     </div>
                                       
