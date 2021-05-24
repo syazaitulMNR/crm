79a615edff043ffda381076b768e586d6830f071
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-24 16:32:15 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -107,7 +107,7 @@
                         <label for="name" class="col-sm-4 col-form-label">Quantity</label>
                         <div class="col-sm-8">
                           {{-- <input type="hidden" class="form-control" name="quantity" value="1" readonly> --}}
-                          <label class="col-form-label">1</label>
+                          {{-- <label class="col-form-label">1</label> --}}
                           <select id="quantity" name="quantity" onchange="calculateAmount(this.value)" class="form-select" required>
                             <option value="" disabled selected>-- Tiket --</option>
                             <option value="1">1</option>
@@ -121,7 +121,7 @@
                         <div class="col-sm-8">
                           {{-- <input type="hidden" class="form-control" name="totalprice" value="{{ $package->price }}" readonly> 
                           <label class="col-form-label">{{$package->price}}</label> --}}
-                          <input type="text" id="totalprice" class="form-control" name="totalprice" style="border: none; outline-width: 0; background-color: none;" readonly>
+                          <input type="text" id="totalprice" class="form-control" name="totalprice" style="border: none; outline-width: 0; background-color: #fff;" readonly>
                         </div>
                     </div>
                                       
