c330b8ae5b987499228104a3a7379b9477359fc6
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-26 00:26:16 +0800

    Update ClosingController.php

--- app/Http/Controllers/ClosingController.php
+++ app/Http/Controllers/ClosingController.php
@@ -11,8 +11,7 @@ class ClosingController extends Controller
     public function view()
     {
         $payment = Payment::orderBy('id','desc')->paginate(15);
-        // dd($payment);
-        // $student = Student::orderBy('id','desc')->get();
-        return view('admin.closing.view', compact('payment'));
+        $student = Student::orderBy('id','desc')->get();
+        return view('admin.closing.view', compact('payment','student'));
     }
 }
