4b6ba71293fc7793eb42e044f4432bc5486c2e0d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-25 23:40:29 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -157,10 +157,12 @@ class ProductController extends Controller
         $feature = Feature::where('product_id', $id)->get();
         $product = Product::where('product_id', $id)->first();
         $package = Package::where('product_id', $id)->paginate(15);
+        $package_id = Package::where('product_id', $id)->get();
 
-        $link = 'https://mims.momentuminternet.my/pendaftaran/'. $product->product_id . '/' . $package->package_id;
-        
-        return view('admin/viewpackage', compact('feature','product', 'package'));   
+        // $link = 'https://mims.momentuminternet.my/pendaftaran/'. $product->product_id . '/' . $package_id->package_id;
+        dd($package_id);
+
+        // return view('admin/viewpackage', compact('feature','product', 'package', 'link'));   
     }
     
     public function pack($id)
