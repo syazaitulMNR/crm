cb217b4440e0c5e0a63e2b86cc1e46697d84f201
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-25 16:00:32 +0800

    Update StudentImport.php

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -76,32 +76,32 @@ class StudentImport implements ToCollection, WithHeadingRow, WithChunkReading
         // }
     }
 
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        // $student = Student::orderBy('id','Desc')->first();
+    // /**
+    // * @param array $row
+    // *
+    // * @return \Illuminate\Database\Eloquent\Model|null
+    // */
+    // public function model(array $row)
+    // {
+    //     // $student = Student::orderBy('id','Desc')->first();
 
-        // $auto_inc = $student->id + 1;
-        // $stud_id = 'MI' . 0 . 0 . $auto_inc;
+    //     // $auto_inc = $student->id + 1;
+    //     // $stud_id = 'MI' . 0 . 0 . $auto_inc;
         
-        if(Student::where('ic', $row['ic'])->exists()){
-        }else{
-            return new Student([
-                // 'stud_id'    => $stud_id,
-                'stud_id'    => $row['stud_id'],
-                'first_name' => $row['first_name'],
-                'last_name'  => $row['last_name'], 
-                'ic'         => $row['ic'],
-                'email'      => $row['email'],
-                'phoneno'    => $row['phoneno'],                
-            ]);
-        }
+    //     if(Student::where('ic', $row['ic'])->exists()){
+    //     }else{
+    //         return new Student([
+    //             // 'stud_id'    => $stud_id,
+    //             'stud_id'    => $row['stud_id'],
+    //             'first_name' => $row['first_name'],
+    //             'last_name'  => $row['last_name'], 
+    //             'ic'         => $row['ic'],
+    //             'email'      => $row['email'],
+    //             'phoneno'    => $row['phoneno'],                
+    //         ]);
+    //     }
         
-    }
+    // }
 
     public function chunkSize(): int
     {
