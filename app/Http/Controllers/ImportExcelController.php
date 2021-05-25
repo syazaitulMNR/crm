98f8af5ad2228772c8b11b9c5b127b5ddb51d4d0
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-25 13:11:29 +0800

    Update ImportExcelController.php

--- app/Http/Controllers/ImportExcelController.php
+++ app/Http/Controllers/ImportExcelController.php
@@ -28,8 +28,8 @@ class ImportExcelController extends Controller
 
     function import()
     {
-        (new StudentImport)->toCollection(request()->file('file'));
-        // (new StudentImport)->import(request()->file('file'));
+        // (new StudentImport)->toCollection(request()->file('file'));
+        (new StudentImport)->import(request()->file('file'));
 
         return back()->with('success', 'The file has been inserted to queue, it may take a while to successfully import.');
     }
