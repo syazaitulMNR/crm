b91263c2adc281fdcf6aacb82da72f16b6e77e2e
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-26 10:42:17 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -220,20 +220,7 @@
       label: {
         connectorAllowed: false
       },
-      pointStart: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ]
+      pointStart: 'Jan'
     }
   },
 
