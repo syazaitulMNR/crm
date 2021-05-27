efb78606be57a1079d4a072b580f1f2dc090129a
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-27 10:15:34 +0800

    Update step2_get1free1same.blade.php

--- resources/views/customer_new/step2_get1free1same.blade.php
+++ resources/views/customer_new/step2_get1free1same.blade.php
@@ -7,12 +7,12 @@ Pendaftaran Pembeli
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-xs-12 px-3 py-5 text-center">
+        <div class="col-sm-12 px-2 py-5 text-center">
             <img src="/assets/images/logo.png" style="max-width:200px">
             <h1 class="display-4 text-dark px-4 pt-3">{{ $product->name }}</h1>
         </div>
 
-        <div class="col-xs-12 d-flex justify-content-center pb-5">
+        <div class="col-sm-12 d-flex justify-content-center pb-5">
             <form action="{{ url('store2') }}/{{ $product->product_id }}/{{ $package->package_id }}" method="POST">
                 @csrf
                 <div class="card w-100 shadow">
