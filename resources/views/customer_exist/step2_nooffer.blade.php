2e0709d7eb634d3607d9c3d05ed60c1a4e4350e8
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-27 10:48:51 +0800

    update

--- resources/views/customer_exist/step2_nooffer.blade.php
+++ resources/views/customer_exist/step2_nooffer.blade.php
@@ -7,9 +7,9 @@ Pendaftaran Pembeli
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 px-3 py-5 text-center">
+        <div class="col-md-12 py-5 text-center">
             <img src="/assets/images/logo.png" style="max-width:200px">
-            <h1 class="text-dark px-4 pt-3">{{ $product->name }}</h1>
+            <h1 class="text-dark pt-3">{{ $product->name }}</h1>
         </div>
 
         <div class="col-md-12 d-flex justify-content-center pb-5">
