9e3612eb697693131173c0a12eaf31e9a246440d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-28 17:01:05 +0800

    Update thankyou.blade.php

--- resources/views/customer/thankyou.blade.php
+++ resources/views/customer/thankyou.blade.php
@@ -39,8 +39,8 @@
         </div> --}}
         
         {{-- <div class="mx-auto py-4"> --}}
-          <div class="embed-responsive embed-responsive-16by9 center py-4">
-            <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <div class="center py-4">
+            <iframe src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
           </div>
         {{-- </div> --}}
  
