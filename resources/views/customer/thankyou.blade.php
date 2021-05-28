c8db425a74bc8241a9333b043be13f34725bdc16
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-28 16:27:05 +0800

    Update thankyou.blade.php

--- resources/views/customer/thankyou.blade.php
+++ resources/views/customer/thankyou.blade.php
@@ -22,7 +22,7 @@
         </div> --}}
         
         {{-- <div class="mx-auto py-4"> --}}
-          <div class="row d-flex justify-content-center py-4" style="width: 400px">
+          <div class="row mx-auto py-4">
             <iframe src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
           </div>
         {{-- </div> --}}
