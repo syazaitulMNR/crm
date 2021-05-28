19795a6127b51e4bc2604ad2bdef0d63f20bb9db
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-28 16:24:34 +0800

    Update thankyou.blade.php

--- resources/views/customer/thankyou.blade.php
+++ resources/views/customer/thankyou.blade.php
@@ -16,21 +16,21 @@
   <div class="col-md-12 px-5 py-4">
     <div class="text-center">
         <h3 class="display-4">Terima Kasih!</h3>
-        <p>Pembelian anda telah berjaya. Sila tonton video ini sampai habis untuk dapatkan info penting untuk tindakan selanjutnya.</p>
+        <p class="lead">Pembelian anda telah berjaya. Sila tonton video ini sampai habis untuk dapatkan info penting untuk tindakan selanjutnya.</p>
         {{-- <div class="py-3" style="font-size: 24px; color: green;">
           <i class="far fa-check-circle fa-8x text-center"></i>
         </div> --}}
         
         {{-- <div class="mx-auto py-4"> --}}
-          <div class="embed-responsive embed-responsive-16by9 center py-4">
-            <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+          <div class="row d-flex justify-content-center py-4">
+            <iframe src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
           </div>
         {{-- </div> --}}
  
         <hr>
         
-        <p> Pengesahan pembelian akan dihantar kepada emel yang telah didaftarkan dalam masa 48 Jam. Terima kasih kerana menunggu. </p>
-        <p>
+        <p class="lead"> Pengesahan pembelian akan dihantar kepada emel yang telah didaftarkan dalam masa 48 Jam. Terima kasih kerana menunggu. </p>
+        <p class="lead">
           Jika terdapat sebarang pertanyaan, sila <a href="https://momentuminternet.com/contactus/" class="link-primary">hubungi kami.</a><br><br>
           {{-- <a class="btn btn-dark py-3 px-4" href="{{ url('updateform')}}/{{$product->product_id}}/{{$package->package_id}}/{{$student->stud_id}}/{{$payment->payment_id}}" role="button">Kemaskini</a> --}}
         </p>
