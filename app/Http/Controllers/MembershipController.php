43ec9cd5f45baee81ce3d9892f7ef48fa3f6c654
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 06:56:46 +0800

    Update MembershipController.php

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -66,7 +66,7 @@ class MembershipController extends Controller
 
     public function view($membership_id, $level_id)
     {
-        $student = Student::where('membership_id', $membership_id)->paginate(15);
+        $student = Student::where('membership_id', $membership_id)->where('level_id', $level_id)->paginate(15);
         $membership = Membership::where('membership_id', $membership_id)->first();
         $membership_level = Membership_Level::where('membership_id', $membership_id)->where('level_id', $level_id)->first();
 
