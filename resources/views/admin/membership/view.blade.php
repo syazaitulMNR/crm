84d740a32830826328c9596d2e8dd050dc324603
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 06:52:54 +0800

    Update view.blade.php

--- resources/views/admin/membership/view.blade.php
+++ resources/views/admin/membership/view.blade.php
@@ -189,7 +189,7 @@
                 </thead>
                 <tbody> 
                   @foreach ($student as $key => $students)   
-                  @if ($students->membership_id == $membership->membership_id)
+                  @if ($students->level_id == $membership->level_id)
                   <tr>
                       <td>{{ $student->firstItem() + $key }}</td>
                       <td>{{ $students->ic }}</td>
