5e32248a6038d48631e1cc1e98fb9a372614ed85
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-28 17:03:32 +0800

    Update thankyou.blade.php

--- resources/views/customer/thankyou.blade.php
+++ resources/views/customer/thankyou.blade.php
@@ -48,7 +48,7 @@
         
         {{-- <div class="mx-auto py-4"> --}}
           <div class="center py-4">
-            <iframe src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="video-size" src="https://player.vimeo.com/video/531964255?color=ffffff&title=0&byline=0&portrait=0&badge=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
           </div>
         {{-- </div> --}}
  
