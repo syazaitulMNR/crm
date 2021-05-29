95df273754da67bf709db95deef8bb0887270903
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 23:07:26 +0800

    Update trackpayment.blade.php

--- resources/views/admin/reports/trackpayment.blade.php
+++ resources/views/admin/reports/trackpayment.blade.php
@@ -147,7 +147,7 @@
                             <div class="col-sm-6">
                                 <button type="submit" class="btn btn-sm btn-success"><i class="fas fa-save pr-1"></i> Save </button>
                                 <a class="btn btn-sm btn-outline-dark" href="{{ url('send-mail') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $payment->payment_id }}/{{ $student->stud_id }}">
-                                    <i class="far fa-envelope pr-1"></i> Purchased Confirmation Mail
+                                    <i class="fas fa-paper-plane pr-1"></i> Purchased Confirmation Mail
                                 </a>
                                 <a class="btn btn-sm btn-outline-dark" href="{{ url('send-mail') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $payment->payment_id }}/{{ $student->stud_id }}">
                                     <i class="fas fa-paper-plane pr-1"></i> Updated Confirmation Mail
