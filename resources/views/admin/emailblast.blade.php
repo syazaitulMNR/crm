92fd68761d0d0245421ce2a91d08951508e83dd0
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 21:22:56 +0800

    Update emailblast.blade.php

--- resources/views/admin/emailblast.blade.php
+++ resources/views/admin/emailblast.blade.php
@@ -52,7 +52,7 @@
                 {{-- <td class="text-center">{{$totalcust}}</td> --}}
                 <td>
                     <a class="btn btn-light" href="{{ url('view') }}/{{ $products->product_id }}"><i class="fas fa-eye"></i></a>
-                    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-paper-plane"></i></button>
+                    {{-- <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-paper-plane"></i></button>
                     <!-- Modal -->
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
@@ -70,7 +70,7 @@
                             </div>
                             </div>
                         </div>
-                    </div>
+                    </div> --}}
                 </td>
             </tr>
             @endforeach
