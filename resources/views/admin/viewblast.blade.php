35310550f49021f4b790b6ea1662da2f517b023c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 22:31:12 +0800

    Update viewblast.blade.php

--- resources/views/admin/viewblast.blade.php
+++ resources/views/admin/viewblast.blade.php
@@ -15,7 +15,7 @@
     </div>
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <h1 class="h2">{{ $product->name }}</h1>
+      <h1 class="h2">{{ $package->name }}</h1>
     </div>
 
     <br>
