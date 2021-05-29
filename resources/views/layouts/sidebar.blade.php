f582abafc58e6bd7730440e6683fab9100d76617
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 11:17:15 +0800

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -71,12 +71,12 @@
               <i class="fas fa-user-tie" style="margin-right: 30px"></i> Customer 
             </a>            
           </li>
-          <li class="nav-item pb-3">
+          {{-- <li class="nav-item pb-3">
             <a class="nav-link text-white bg-dark " href="/membership">
               <span data-feather="home"></span>
               <i class="fas fa-gem" style="margin-right: 28px"></i> Membership 
             </a>            
-          </li>
+          </li> --}}
           {{-- <li class="nav-item"> 
             <a class="nav-link dropdown-toggle text-white bg-dark active" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-clipboard-list"  style="margin-right: 32px"></i> Order History
