2b043c3a2aa555383ccb83ef44aff035dafd5454
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 20:17:26 +0800

    Update viewblast.blade.php

--- resources/views/admin/viewblast.blade.php
+++ resources/views/admin/viewblast.blade.php
@@ -20,7 +20,7 @@
 
     {{--r {{$totalcust}} --}}
 
-    <div class="float-right pt-3">{{$payment->links()}}</div>
+    Showing {{$services->count()}} of {{$services->total()}}<div class="float-right pt-3">{{$payment->links()}}</div>
     @if(count($payment) > 0)
 
     <table class="table table-hover">
