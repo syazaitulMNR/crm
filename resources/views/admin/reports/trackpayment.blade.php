714a98bfef1ab73a3240d03e7561d1cf7e49841e
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 18:34:12 +0800

    Update trackpayment.blade.php

--- resources/views/admin/reports/trackpayment.blade.php
+++ resources/views/admin/reports/trackpayment.blade.php
@@ -64,27 +64,27 @@
                             <div class="mb-3 row">
                                 <label class="col-sm-2">IC No.</label>
                                 <div class="col-sm-4">
-                                    <input type="text" class="form-control" name="ic" value="{{ $student->ic }}">
+                                    : &nbsp;&nbsp;&nbsp; <input type="text" class="form-control" name="ic" value="{{ $student->ic }}">
                                     {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->ic }}</p> --}}
                                 </div>
 
                                 <label class="col-sm-2">Phone No.</label>
                                 <div class="col-sm-4">
-                                    <input type="text" class="form-control" name="phoneno" value="{{ $student->phoneno }}">
+                                    : &nbsp;&nbsp;&nbsp; <input type="text" class="form-control" name="phoneno" value="{{ $student->phoneno }}">
                                     {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->phoneno }}</p> --}}
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">Name</label>
                                 <div class="col-sm-4">
-                                    <input type="text" class="form-control" name="first_name" value="{{ $student->first_name }}" placeholder="First Name">
-                                    <input type="text" class="form-control" name="last_name" value="{{ $student->last_name }}" placeholder="Last Name">
+                                    : &nbsp;&nbsp;&nbsp; <input type="text" class="form-control" name="first_name" value="{{ $student->first_name }}" placeholder="First Name">
+                                    &nbsp;&nbsp;&nbsp;&nbsp; <input type="text" class="form-control" name="last_name" value="{{ $student->last_name }}" placeholder="Last Name">
                                     {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->first_name }}&nbsp;{{ $student->last_name }}</p> --}}
                                 </div>
 
                                 <label class="col-sm-2">Email Address</label>
                                 <div class="col-sm-4">
-                                    <input type="text" class="form-control" name="email" value="{{ $student->email }}">
+                                    : &nbsp;&nbsp;&nbsp; <input type="text" class="form-control" name="email" value="{{ $student->email }}">
                                     {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->email }}</p> --}}
                                 </div>
                             </div>
