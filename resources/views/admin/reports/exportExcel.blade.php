f22515eec18fe63d2b3fe2cf155979c97770bf89
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-29 16:56:15 +0800

    Update exportExcel.blade.php

--- resources/views/admin/reports/exportExcel.blade.php
+++ resources/views/admin/reports/exportExcel.blade.php
@@ -12,6 +12,7 @@
     <th>Status</th>
     <th>Payment Method</th>
     <th>Package</th>
+    <th>Purchased At</th>
 
 </tr>
 </thead>
@@ -33,6 +34,7 @@
         <td>{{ $payments->status }}</td>
         <td>{{ $payments->pay_method }}</td>
         <td>{{ $packages->name }}</td>
+        <td>{{  date('d-m-Y h:i a', strtotime($payments->created_at))  }}</td>
     </tr>
 @endif
 @endif
