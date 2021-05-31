92e8995949d94b986ad1239278472cca466ecd58
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-31 10:25:03 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -49,7 +49,69 @@
         </div>
 
         <div class="row">
-          <div class="col-md-9 "> 
+          <div class="col-md-12 "> 
+            
+            {{-- <div class="row">
+              @foreach ($package as $packages)
+              <div class="col-md-4">
+                <a class="btn bg-dark btn-lg text-white text-center" style="width: 100%" href="{{ url('viewbypackage') }}/{{ $product->product_id }}/{{ $packages->package_id }}">
+                  <h6 class="pt-3 pb-2">{{$packages->name}}</h6>
+                </a>
+              </div>
+              @endforeach
+            </div> --}}
+            
+            <br>
+
+            <!-- Show data in cards --------------------------------------------------->
+            <div class="row mb-3">
+              <div class="col-xl-3 col-lg-6">
+                <div class="card bg-light card-inverse shadow">
+                  <div class="card-block">
+                    <div class="rotate">
+                      <i class="fas fa-dollar-sign fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
+                    </div>
+                    <h6 class="lead pt-3 pl-3">Success Payment</h6>
+                    <h3 class="pb-1 pl-3">{{$totalsuccess}}</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-6">
+                <div class="card bg-light card-inverse shadow">
+                  <div class="card-block">
+                    <div class="rotate">
+                      <i class="fa fas fa-dollar-sign fa-6x" style="color: rgba(255, 0, 0, 0.3)"></i>
+                    </div>
+                    <h6 class="lead pt-3 pl-3">Due Payment</h6>
+                    <h3 class="pb-1 pl-3">{{$totalcancel}}</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-6">
+                <div class="card bg-light card-inverse shadow">
+                  <div class="card-block">
+                    <div class="rotate">
+                      <i class="fas fa-ticket-alt fa-6x" style="color: rgba(17, 0, 255, 0.3)"></i>
+                    </div>
+                    <h6 class="lead pt-3 pl-3">Paid Ticket</h6>
+                    <h3 class="pb-1 pl-3">{{$paidticket}}</h3>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-6">
+                <div class="card bg-light card-inverse shadow">
+                  <div class="card-block">
+                    <div class="rotate">
+                      <i class="fas fa-ticket-alt fa-6x" style="color: rgba(0, 221, 255, 0.3)"></i>
+                    </div>
+                    <h6 class="lead pt-3 pl-3">Free Ticket</h6>
+                    <h3 class="pb-1 pl-3">{{$freeticket}}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <br>
 
             <!-- Show package in table ----------------------------------------------->
             @if(count($package) > 0)
@@ -79,52 +141,80 @@
             <p>There are no package yet.</p>
             @endif
             <div class="float-right pt-3">{{$package->links()}}</div>
+
+            {{-- @if(count($student) > 0)
+            <table class="table table-hover" id="successTable">
+                <thead>
+                <tr class="header">
+                    <th>#</th>
+                    <th>IC No.</th>
+                    <th>Name</th>
+                    {{-- <th>Payment (RM)</th> 
+                    <th>Status</th>
+                    <th>Purchase Date</th>
+                    <th><i class="fas fa-cogs"></i></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($student as $students)    
+                @foreach ($payment as $payments)
+                @if ($payments->stud_id == $students->stud_id)
+                @if ($product->product_id == $payments->product_id)  
+                <tr>
+                    <td>{{ $students->stud_id }}</td>
+                    <td>{{ $students->ic }}</td>
+                    <td>{{ $students->first_name }}</td>
+                    {{-- <td>RM {{ $payments->totalprice }}</td> 
+                    <td>
+                      @if ($payments->status == 'paid')
+                        <span class="badge rounded-pill bg-success"> &nbsp;{{ $payments->status }}&nbsp; </span>
+                      @elseif ($payments->status == 'due')
+                        <span class="badge rounded-pill bg-danger"> &nbsp;{{ $payments->status }}&nbsp; </span>
+                      @else
+                        <p>NULL</p>
+                      @endif
+                    </td>
+                    <td>{{ date('d/m/Y', strtotime($payments->created_at)) }}</td>
+                    <td>
+                      <a class="btn btn-primary" href="{{ url('viewpayment') }}/{{ $product->product_id }}/{{ $payments->package_id }}/{{ $payments->payment_id }}/{{ $payments->stud_id }}"><i class="fas fa-edit"></i></a>
+                    </td>
+                </tr>
+                @endif
+                @endif
+                @endforeach
+                @endforeach
+                </tbody>
+            </table>  
+            @else
+            <p>There are no any success payment yet.</p>
+            @endif
+            <div class="float-right pt-3">{{$student->links()}}</div>    --}}
             
           </div>
           
-          <div class="col-md-3">
+          {{-- <div class="col-md-3">
 
-            <div class="card bg-light py-2 mb-2 text-center shadow">
-              <div class="card-block text-dark">
-                <div class="rotate">
-                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
-                </div>
-                <h3 class="pt-3 pl-3">{{$totalsuccess}}</h3>
-                <h6 class="lead pb-2 pl-3">Success Payment</h6>
+            <div class="card mb-4 text-center">
+              <div class="card-body pt-4">
+                <i class="far fa-check-circle fa-3x" style="color:rgb(69, 139, 95)"></i>
+                <h3 class="pt-4">{{$totalsuccess}}</h3>
+              </div>
+              <div class="card-footer">
+                <h6>Total Paid</h6>
               </div>
             </div>
-
-            <div class="card bg-light py-2 mb-2 text-center shadow">
-              <div class="card-block text-dark">
-                <div class="rotate">
-                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
-                </div>
-                <h3 class="pt-3 pl-3">{{$totalcancel}}</h3>
-                <h6 class="lead pb-2 pl-3">Due Payment</h6>
+    
+            <div class="card mb-4 text-center">
+              <div class="card-body pt-4">
+                <i class="far fa-times-circle fa-3x" style="color:rgb(240, 0, 0)"></i>
+                <h3 class="pt-4">{{$totalcancel}}</h3>
               </div>
-            </div>
-
-            <div class="card bg-light py-2 mb-2 text-center shadow">
-              <div class="card-block text-dark">
-                <div class="rotate">
-                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(0, 255, 94, 0.3)"></i>
-                </div>
-                <h3 class="pt-3 pl-3">{{$paidticket}}</h3>
-                <h6 class="lead pb-2 pl-3">Paid Ticket</h6>
-              </div>
-            </div>
-
-            <div class="card bg-light py-2 mb-2 text-center shadow">
-              <div class="card-block text-dark">
-                <div class="rotate">
-                  <i class="fa fas fa-dollar-sign fa-6x" style="color:rgba(255, 0, 0, 0.3)"></i>
-                </div>
-                <h3 class="pt-3 pl-3">{{$freeticket}}</h3>
-                <h6 class="lead pb-2 pl-3">Free Ticket</h6>
+              <div class="card-footer">
+                <h6>Total Due</h6>
               </div>
             </div>
           
-          </div>
+          </div> --}}
           
         </div>
         
