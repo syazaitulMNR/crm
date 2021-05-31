1c74670de02e43bd5d0c6a4dcd920f016daaf6b9
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-05-31 12:12:13 +0800

    update

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -244,7 +244,7 @@ class ReportsController extends Controller
         $student_id = Student::where('ic', $request->search)->first();
         $stud_id = $student_id->stud_id;
 
-        $pay_data = Payment::where('student_id','LIKE','%'. $stud_id.'%')->get();
+        $pay_data = Payment::where('stud_id','LIKE','%'. $stud_id.'%')->get();
 
         // $stud = Student::where('name','LIKE','%'. $request->search.'%')->orWhere('ic','LIKE','%'. $request->search .'%')->get();
         // $pay = Payment::where('stud_id','LIKE','%'. $request->search.'%')->orWhere('status','LIKE','%'. $request->search .'%')->get();
