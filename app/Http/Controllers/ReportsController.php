48eacbad4a8d7da5fe47f05e9fbea736d5b3de7f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-01 15:13:57 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -244,25 +244,22 @@ class ReportsController extends Controller
 
         //get details from search
         $student_id = Student::where('ic', $request->search)->orWhere('first_name', $request->search)->orWhere('last_name', $request->search)->orWhere('email', $request->search)->first();
-        
-        if ($student_id = NULL){
+        $stud_id = $student_id->stud_id;
 
-            return redirect()->back()->with('search-error', 'Customer not found!');
+        $payment = Payment::where('stud_id','LIKE','%'. $stud_id.'%')->get();
+
+        // dd($stud_id);
+        // $stud = Student::where('name','LIKE','%'. $request->search.'%')->orWhere('ic','LIKE','%'. $request->search .'%')->get();
+        // $pay = Payment::where('stud_id','LIKE','%'. $request->search.'%')->orWhere('status','LIKE','%'. $request->search .'%')->get();
+
+        if(count($payment) > 0)
+        {
+            return view('admin.reports.viewbypackage', compact('product', 'package', 'payment', 'student', 'count', 'total', 'totalsuccess', 'totalcancel', 'paidticket', 'freeticket'));
 
         }else{
 
-            $stud_id = $student_id->stud_id;
-            $payment = Payment::where('stud_id','LIKE','%'. $stud_id.'%')->get();
+            return redirect()->back()->with('search-error', 'Customer not found!');
 
-            if(count($payment) > 0)
-            {
-                return view('admin.reports.viewbypackage', compact('product', 'package', 'payment', 'student', 'count', 'total', 'totalsuccess', 'totalcancel', 'paidticket', 'freeticket'));
-
-            }else{
-
-                return redirect()->back()->with('search-error', 'Customer not found!');
-
-            }
         }
     }
 
