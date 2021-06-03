8cb0bd7767a52b885695ba784572e9291e84d3ea
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-03 10:49:16 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -137,9 +137,9 @@ Route::get('viewbypackage/{product_id}/{package_id}', 'ReportsController@viewbyp
 Route::get('delete/{payment_id}/{product_id}/{package_id}', 'ReportsController@destroy');
 
 Route::get('paid-ticket/{product_id}/{package_id}', 'ReportsController@paid_ticket');
-Route::get('paid-ticket/view/{product_id}/{package_id}', 'ReportsController@track_paid');
+Route::get('paid-ticket/view/{product_id}/{package_id}/{ticket_id}', 'ReportsController@track_paid');
 Route::get('free-ticket/{product_id}/{package_id}', 'ReportsController@free_ticket');
-Route::get('free-ticket/view/{product_id}/{package_id}', 'ReportsController@track_free');
+Route::get('free-ticket/view/{product_id}/{package_id}/{ticket_id}', 'ReportsController@track_free');
 
 Route::post('new-customer/save/{product_id}/{package_id}', 'ReportsController@save_customer');
 Route::get('viewpayment/{product_id}/{package_id}/{payment_id}/{student_id}', 'ReportsController@trackpayment');
