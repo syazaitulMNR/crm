6a7da146ad1edf2127ca60373fe54f19dd9336c1
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-06 09:51:07 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -87,9 +87,7 @@ class HomeController extends Controller
         $package = Package::where('package_id', $package_id)->first();
         $product = Product::where('product_id', $product_id)->first();
 
-        // // return view('customer/main', compact('product', 'package')); bukan yg ni taw!!!
-        return view('customer_new/check_ic', compact('product', 'package'));
-        // abort(404); 
+        return view('customer.update_participant', compact('product', 'package'));
     }
 
     public function verify_ic($product_id, $package_id, Request $request)
