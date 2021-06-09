2a30c17dffa5a71bb40bcacdf873478ec7ae37c4
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-09 14:57:15 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -103,8 +103,10 @@
       <hr class="my-3">
 
       <!-- Show data in bar chart --------------------------------------------------->
-      <div class="card bg-white card-inverse shadow px-2 py-2">
-        <div id="chartdata" ></div>
+      <div class="col-md-12">
+        <div class="card bg-white shadow px-2 py-2">
+          <div id="chartdata" ></div>
+        </div>
       </div>
 
       <!-- Show data in line graph --------------------------------------------------->
