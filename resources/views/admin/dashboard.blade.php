05c40f341168dbc5d81b3867e4b202f465ae13de
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-09 16:09:06 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -109,7 +109,7 @@
 
             <h5 class="text-center pb-4">Roket Pemasaran Momentum Copywriting 2021</h5>
 
-            <table class="table table-bordered text-center">
+            <table class="table text-center">
               <thead class="thead">
                 <tr>
                   <th class="text-left">Package</th>
