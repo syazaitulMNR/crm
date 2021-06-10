4da607903c3060a379a9743ec9e2f382f1dc1dcf
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-10 16:52:27 +0800

    Update importexcel.blade.php

--- resources/views/admin/reports/importexcel.blade.php
+++ resources/views/admin/reports/importexcel.blade.php
@@ -36,9 +36,8 @@
             <div class="input-group p-3">
                 <input type="file" name="file" class="form-control" required>
                 <button class="btn btn-dark"><i class="fas fa-upload pt-1"></i></button>
-                <br>
-                <em>Maximum upload file size: 8MB</em>
             </div>
+            <em class="pl-1">Maximum upload file size: 8MB</em>
 
         </form>
            
