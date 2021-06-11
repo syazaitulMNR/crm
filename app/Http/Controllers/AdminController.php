6a9fa444edd3d7d9636f3dd4e1eeca19902e73a1
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-11 15:53:47 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,10 +71,14 @@ class AdminController extends Controller
 
         // Report Table
         $product = Product::where('status', 'active')->first();
-        $package = Package::where('product_id', $product->product_id)->get();
+        $product_id = $product->product_id;
+
+        $package = Package::where('product_id', $product_id)->get();
+        $registration = Payment::where('status','paid')->where('product_id', $product_id)->count();
+        $ticket = Ticket::where('product_id', $product_id)->count();
         
         // dd($times);
-        return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package'));
+        return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package', 'registration', 'ticket'));
     }
 
     /*-- Manage User --------------------------------------------------------*/
