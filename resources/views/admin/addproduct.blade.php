295087eb7a19f7e79f3ae84b2a5bf83816d344c3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-11 11:51:45 +0800

    Update addproduct.blade.php

--- resources/views/admin/addproduct.blade.php
+++ resources/views/admin/addproduct.blade.php
@@ -66,9 +66,9 @@
                     <div class='col-md-8'>     
                         <label for="name">Date</label>
                         <div class="form-group row pl-3">
-                            <input type="text" name="date1" id="date1" class="form-control" style="width:46%" placeholder="Date From" required/>
+                            <input type="text" name="date1" id="date1" class="form-control" placeholder="Date From" required/>
                             &nbsp;&nbsp;&nbsp; To &nbsp;&nbsp;&nbsp;
-                            <input type="text" name="date2" id="date2" class="form-control" style="width:46%" placeholder="Date To" required/>
+                            <input type="text" name="date2" id="date2" class="form-control" placeholder="Date To" required/>
                         </div>
                     </div>
 
