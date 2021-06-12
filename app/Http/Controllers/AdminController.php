f7fca6c4f0276c1ccbfa382b454b955b2621c684
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-12 12:36:50 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -78,7 +78,7 @@ class AdminController extends Controller
         // $registration = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package_id)->count();
 
         $date_today = date('d-m-Y');
-        $current_time = Carbon::now('Asia/Kuala_Lumpur')->createFromTime($hour, $minute);
+        $current_time = Carbon::createFromTime($hour, $minute);
 
         if ($current_time < "8") {
 
