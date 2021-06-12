c19dbf17cdf0535dd0fc14b291c3e7660a6a5f03
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-12 16:09:43 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -128,10 +128,7 @@ class AdminController extends Controller
         $package = Package::where('product_id', $product_id)->get();
         $package_id = Package::where('product_id', $product_id)->pluck('package_id');
 
-        // foreach($package_id as $key => $value)
-        // {
-            $registration = Payment::where('status','paid')->where('product_id', $product_id)->whereIn('package_id', $package_id)->count();
-        // }
+        $registration = Payment::whereIn('package_id', $package_id)->count();
         $paidticket = Ticket::where('ticket_type', 'paid')->where('product_id', $product_id)->whereIn('package_id', $package_id)->count();
         $freeticket = Ticket::where('ticket_type', 'free')->where('product_id', $product_id)->whereIn('package_id', $package_id)->count();
 
@@ -142,8 +139,8 @@ class AdminController extends Controller
         $totalticket = Ticket::where('product_id', $product_id)->count();
         
         // dd($package_id[0]);
-        // dd($package_id);
-        return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package', 'date_today', 'current_time', 'duration', 'registration', 'paidticket', 'freeticket', 'totalregister', 'totalpaid', 'totalfree', 'totalticket'));
+        dd($registration);
+        // return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package', 'date_today', 'current_time', 'duration', 'registration', 'paidticket', 'freeticket', 'totalregister', 'totalpaid', 'totalfree', 'totalticket'));
     }
 
     /*-- Manage User --------------------------------------------------------*/
