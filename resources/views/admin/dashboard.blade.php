655a950fdd0bdb16ad597bb2fdc31dc580815bfd
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-12 15:22:33 +0800

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -124,10 +124,10 @@
               @foreach ($package as $packages)
               {{-- @foreach ($registration as $key => $register) --}}
                 <tr>
-                  <td class="text-left">{{ $packages->name }}{{ $packages->package_id }}</td>
+                  <td class="text-left">{{ $packages->name }}</td>
                   <td>
                     {{ number_format($registration) }}
-                    <input name="package_id" type="hidden" class="form-control" value="{{ $packages->package_id }}">
+                    <input name="package_id" type="text" value="{{ $packages->package_id }}">
                   </td>
                   <td>{{ number_format($paidticket) }}</td>
                   <td>{{ number_format($freeticket) }}</td>
