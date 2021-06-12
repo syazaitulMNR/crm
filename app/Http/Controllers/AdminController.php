9406861a62b4320a8a167dc7d6f7b18cb64d3d66
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-12 09:26:39 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -76,7 +76,7 @@ class AdminController extends Controller
         $package = Package::where('product_id', $product_id)->get();
         // $registration = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package_id)->count();
 
-        $current_time = date('Y-m-d H:i:s', strtotime('+8 hours'));
+        $current_time = date('Y-m-d H:i a', strtotime('+8 hours'));
         $totalregister = Payment::where('status','paid')->where('product_id', $product_id)->count();
         $totalpaid = Ticket::where('ticket_type', 'paid')->where('product_id', $product_id)->count();
         $totalfree = Ticket::where('ticket_type', 'free')->where('product_id', $product_id)->count();
