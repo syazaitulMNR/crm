f83f6f4aa488da30c04840473097b3b3c1801da7
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-12 14:18:44 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -142,8 +142,8 @@ class AdminController extends Controller
         $totalticket = Ticket::where('product_id', $product_id)->count();
         
         // dd($package_id[0]);
-        dd($package_id[$key]);
-        // return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package', 'date_today', 'current_time', 'duration', 'registration', 'paidticket', 'freeticket', 'totalregister', 'totalpaid', 'totalfree', 'totalticket'));
+        // dd($package_id[$key]);
+        return view('admin.dashboard', compact('student','today','monthly','yearly','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec','greetings', 'product', 'package', 'date_today', 'current_time', 'duration', 'registration', 'paidticket', 'freeticket', 'totalregister', 'totalpaid', 'totalfree', 'totalticket'));
     }
 
     /*-- Manage User --------------------------------------------------------*/
