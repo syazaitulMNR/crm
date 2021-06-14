a743929f53b3a4be6ab4de5402e7ed74a6a3b251
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-14 12:21:57 +0800

    Update import.blade.php

--- resources/views/admin/membership/import.blade.php
+++ resources/views/admin/membership/import.blade.php
@@ -41,70 +41,87 @@
         <br>
         <div class="panel panel-default">
 
-            <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
-                <!-- create campaign -->
-                <div class="btn-group" role="group" aria-label="First group">
-                    <h5>How To Import ?</h5>
+            <h5 class="pt-3 pb-2">How To Import ?</h5>
+
+            <p>1) Please download this format before import to database.</p>
+            <div class="row">
+                <div class="col-md-12">
+                    {{-- <div class="card bg-light"> --}}
+                        <div class="table-responsive">
+                            <!-- Show details in table ----------------------------------------------->
+                            <table class="table table-bordered table-sm" id="myTable">
+                                <tr class="table-active">
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>IC No.</th>
+                                    <th>Email</th>
+                                    <th>Phone No</th>
+                                    <th>Price (RM)</th>
+                                    <th>Quantity</th>
+                                    <th>Total Payment</th>
+                                    <th>Pay Method</th>
+                                    {{-- <th>Ticket Type</th> --}}
+                                    <th>Offer ID</th>
+                                    <th>User ID</th>
+                                </tr>
+                                <tr>
+                                    <td>John</td>
+                                    <td>Doe</td>
+                                    <td>900101014321</td>
+                                    <td>example@gmail.com</td>
+                                    <td>+60123456789</td>
+                                    <td>199</td>
+                                    <td>1</td>
+                                    <td>199</td>
+                                    <td>FPX</td>
+                                    {{-- <td>free</td> --}}
+                                    <td>OFF001</td>
+                                    <td>UID001</td>
+                                </tr>
+                            </table>
+                        </div>
+                    {{-- </div> --}}
+    
+                    <div class="row-fluid float-right pt-1">
+                        <a class="btn btn-warning" href="{{ url('exportExcel') }}/{{ $product->product_id }}/{{ $package->package_id }}"><i class="fas fa-download pr-2"></i>Download</a>
+                    </div>
                 </div>
                 
-                {{-- <div class="input-group">
-                    <a class="btn btn-warning" href="{{ url('exportExcel') }}"><i class="fas fa-download pt-1"></i></a>
-                </div> --}}
             </div>
-            {{-- <input type="text" id="myInput" class="form-control" onkeyup="myFunction()" placeholder="Please Enter IC Number" title="Type in a name"> --}}
-         
-            {{-- <div class="row float-right pt-3">
-                {{-- <div class="col-auto">
-                    <div>{{$data->links()}}</div>
-                </div>
-                <div class="col-auto pt-1">
-                    <a class="btn btn-warning" href="{{ url('exportExcel') }}"><i class="fas fa-download pr-1"></i>Download the format</a>
-                </div>
-            </div> --}}
             
-            <br>  
-
-            <div class="panel-body">
-                <p class="py-1">1) Please download this format before import to database.</p>
-                <div class="table-responsive">
-                    <!-- Show details in table ----------------------------------------------->
-                    <table class="table table-hover" id="myTable">
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>IC No.</th>
-                            <th>Email</th>
-                            <th>Phone No</th>
-                            <th>Price (RM)</th>
-                            <th>Quantity</th>
-                            <th>Total Payment</th>
-                            <th>Pay Method</th>
-                            <th>Offer ID</th>
-                            <th>User ID</th>
-                        </tr>
-                        <tr>
-                            <td>John</td>
-                            <td>Doe</td>
-                            <td>900101014321</td>
-                            <td>example@gmail.com</td>
-                            <td>+60123456789</td>
-                            <td>199</td>
-                            <td>1</td>
-                            <td>199</td>
-                            <td>FPX</td>
-                            <td>OFF001</td>
-                            <td>UID001</td>
-                        </tr>
-                    </table>
+            <p>2) Just refer to this table for Offer ID column.</p>
+            <div class="row">
+                <div class="col-md-12">
+                    {{-- <div class="card bg-light"> --}}
+                        <div class="table-responsive">
+                            <!-- Show details in table ----------------------------------------------->
+                            <table class="table table-bordered table-sm">
+                                <tr class="table-active">
+                                    <th>#</th>
+                                    <th>Offer ID</th>
+                                    <th>Description</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>OFF001</td>
+                                    <td>No Offer</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>OFF002</td>
+                                    <td>Buy 1 Free 1 (Same Ticket)</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>OFF003</td>
+                                    <td>Bulk Ticket</td>
+                                </tr>
+                            </table>
+                        </div>
+                    {{-- </div> --}}
                 </div>
+                
             </div>
-
-            <div class="row float-right pt-3">
-                <div class="col-auto pt-1">
-                    <a class="btn btn-warning" href="{{ url('export-format') }}/{{ $membership->membership_id }}/{{ $membership_level->level_id }}"><i class="fas fa-download pr-2"></i>Download</a>
-                </div>
-            </div>
-
         </div>
     </main>
 </div>
