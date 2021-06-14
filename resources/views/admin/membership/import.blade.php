c5536b36e5f7b1855b86add49d23393520dc8592
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-14 12:25:13 +0800

    Update import.blade.php

--- resources/views/admin/membership/import.blade.php
+++ resources/views/admin/membership/import.blade.php
@@ -56,13 +56,6 @@
                                     <th>IC No.</th>
                                     <th>Email</th>
                                     <th>Phone No</th>
-                                    <th>Price (RM)</th>
-                                    <th>Quantity</th>
-                                    <th>Total Payment</th>
-                                    <th>Pay Method</th>
-                                    {{-- <th>Ticket Type</th> --}}
-                                    <th>Offer ID</th>
-                                    <th>User ID</th>
                                 </tr>
                                 <tr>
                                     <td>John</td>
@@ -70,13 +63,6 @@
                                     <td>900101014321</td>
                                     <td>example@gmail.com</td>
                                     <td>+60123456789</td>
-                                    <td>199</td>
-                                    <td>1</td>
-                                    <td>199</td>
-                                    <td>FPX</td>
-                                    {{-- <td>free</td> --}}
-                                    <td>OFF001</td>
-                                    <td>UID001</td>
                                 </tr>
                             </table>
                         </div>
@@ -89,39 +75,6 @@
                 
             </div>
             
-            <p>2) Just refer to this table for Offer ID column.</p>
-            <div class="row">
-                <div class="col-md-12">
-                    {{-- <div class="card bg-light"> --}}
-                        <div class="table-responsive">
-                            <!-- Show details in table ----------------------------------------------->
-                            <table class="table table-bordered table-sm">
-                                <tr class="table-active">
-                                    <th>#</th>
-                                    <th>Offer ID</th>
-                                    <th>Description</th>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>OFF001</td>
-                                    <td>No Offer</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>OFF002</td>
-                                    <td>Buy 1 Free 1 (Same Ticket)</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>OFF003</td>
-                                    <td>Bulk Ticket</td>
-                                </tr>
-                            </table>
-                        </div>
-                    {{-- </div> --}}
-                </div>
-                
-            </div>
         </div>
     </main>
 </div>
