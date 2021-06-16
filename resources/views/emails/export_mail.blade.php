ea1f5b37794527d406253d3628a64d4b02ffda4a
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-16 15:59:05 +0800

    Update export_mail.blade.php

--- resources/views/emails/export_mail.blade.php
+++ resources/views/emails/export_mail.blade.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8" />
   </head>
   <body>
-
-    <h2>{{ $product->name }} - {{ $package_name->name }} </h2>
+    
     <p>Hello,</p>
     <p>Please download the attachment below to get the details.</p>
     <p>Thank you!</p>
