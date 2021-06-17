b47a17c0eeb7e00bbc6d35991ea07225dd5a1a6e
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-17 15:08:30 +0800

    Update view_member.blade.php

--- resources/views/admin/membership/view_member.blade.php
+++ resources/views/admin/membership/view_member.blade.php
@@ -102,7 +102,7 @@
                                   <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
                                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <div class="modal-body">
+                                <div class="modal-body text-start">
                                   <p>This action will remove the details from the table :</p>
                                   <ul>
                                     <li>Student</li>
