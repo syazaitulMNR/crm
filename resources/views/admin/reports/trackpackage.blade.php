0753f9cc6b8fedbf7c668e54902d4084ca3400bc
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-17 11:52:37 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -75,25 +75,25 @@
 
             <!-- Show data in cards --------------------------------------------------->
             <div class="row mb-3">
-              <div class="col-xl-3 col-lg-6 pb-1">
+              <div class="col-xl-3 col-lg-6 py-2">
                 <div class="card border-0 gradient-1 shadow text-center">
                   <h6 class="pt-3">Paid Ticket</h6>
                   <b class="display-6 pb-3">{{ number_format($totalsuccess) }}</b>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-6 pb-1">
+              <div class="col-xl-3 col-lg-6 py-2">
                 <div class="card border-0 gradient-2 shadow text-center">
                   <h6 class="pt-3">Free Ticket</h6>
                   <b class="display-6 pb-3">{{ number_format($freeticket) }}</b>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-6 pb-1">
+              <div class="col-xl-3 col-lg-6 py-2">
                 <div class="card border-0 gradient-3 shadow text-center">
                   <h6 class="pt-3">Updated Participant</h6>
                   <b class="display-6 pb-3">{{ number_format($paidticket) }}</b>
                 </div>
               </div>
-              <div class="col-xl-3 col-lg-6 pb-1">
+              <div class="col-xl-3 col-lg-6 py-2">
                 <div class="card border-0 gradient-4 shadow text-center">
                   <h6 class="pt-3">Pending Payment</h6>
                   <b class="display-6 pb-3">{{ number_format($totalcancel) }}</b>
@@ -101,7 +101,6 @@
               </div>
             </div>
 
-            <br>
 
             <!-- Show package in table ----------------------------------------------->
             @if(count($package) > 0)
