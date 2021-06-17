65f36c4364557393101156de21a66fad460f8547
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-17 11:43:06 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -80,25 +80,25 @@
               <div class="col-xl-3 col-lg-6">
                 <div class="card border-0 gradient-1 shadow text-center">
                   <h6 class="pt-3">Paid Ticket</h6>
-                  <b class="display-6 pb-1">{{ number_format($totalsuccess) }}</b>
+                  <b class="display-6 pb-3">{{ number_format($totalsuccess) }}</b>
                 </div>
               </div>
               <div class="col-xl-3 col-lg-6">
                 <div class="card border-0 gradient-2 shadow text-center">
                   <h6 class="pt-3">Free Ticket</h6>
-                  <b class="display-6 pb-1">{{ number_format($freeticket) }}</b>
+                  <b class="display-6 pb-3">{{ number_format($freeticket) }}</b>
                 </div>
               </div>
               <div class="col-xl-3 col-lg-6">
                 <div class="card border-0 gradient-3 shadow text-center">
                   <h6 class="pt-3">Updated Participant</h6>
-                  <b class="display-6 pb-1">{{ number_format($paidticket) }}</b>
+                  <b class="display-6 pb-3">{{ number_format($paidticket) }}</b>
                 </div>
               </div>
               <div class="col-xl-3 col-lg-6">
                 <div class="card border-0 gradient-4 shadow text-center">
                   <h6 class="pt-3">Pending Payment</h6>
-                  <b class="display-6 pb-1">{{ number_format($totalcancel) }}</b>
+                  <b class="display-6 pb-3">{{ number_format($totalcancel) }}</b>
                 </div>
               </div>
             </div>
