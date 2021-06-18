c95285db050b2279bfb342999d05d3f30af0b4e3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-18 15:34:14 +0800

    Update trackticket.blade.php

--- resources/views/admin/reports/trackticket.blade.php
+++ resources/views/admin/reports/trackticket.blade.php
@@ -181,7 +181,7 @@
                     <div class="row-fluid text-right">                        
                         @if(Auth::user()->role_id == 'ROD003' || Auth::user()->role_id == 'ROD004')
                         @else
-                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $ticket->ticket_id }}"><i class="fas fa-trash-alt"></i></button>
+                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $ticket->ticket_id }}"><i class="fas fa-trash-alt"></i> Delete</button>
                         <!-- Modal -->
                         <div class="modal fade" id="exampleModal{{ $ticket->ticket_id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered">
