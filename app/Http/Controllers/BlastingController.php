e4a48867bc1b1f69fe821d32a56d119f5be4654f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-19 12:16:37 +0800

    Update BlastingController.php

--- app/Http/Controllers/BlastingController.php
+++ app/Http/Controllers/BlastingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Jobs\BlastQueueEmail;
 use App\Jobs\PengesahanJob;
+use App\Jobs\TiketJob;
 use App\Student;
 use App\Product;
 use App\Package;
