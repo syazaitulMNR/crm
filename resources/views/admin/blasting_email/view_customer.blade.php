5279e534425a2954a1c2af5555d2ee4da6e7306d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-19 16:51:48 +0800

    Update view_customer.blade.php

--- resources/views/admin/blasting_email/view_customer.blade.php
+++ resources/views/admin/blasting_email/view_customer.blade.php
@@ -39,7 +39,7 @@
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">   
         
         <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-            <a href="{{ url('view')}}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/emailblast">Email Blasting</a> 
+            <a href="{{ url('view-event')}}/{{ $product->product_id }}/{{ $package->package_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/emailblast">Email Blasting</a> 
             / <a href="{{ url('view')}}/{{ $product->product_id }}"> {{ $product->name }} </a> / <a href="{{ url('view-event')}}/{{ $product->product_id }}/{{ $package->package_id }}">{{ $package->name }}</a>
             / <b> {{ $student->first_name }} </b>
         </div>
