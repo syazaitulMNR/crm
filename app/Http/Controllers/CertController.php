27bc4f2d1f8a283c3e41dc0ee2a7c0e1a43fa9aa
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-20 22:21:11 +0800

    Update CertController.php

--- app/Http/Controllers/CertController.php
+++ app/Http/Controllers/CertController.php
@@ -33,29 +33,26 @@ class CertController extends Controller
             $check_payment = Payment::where('stud_id', $student->stud_id)->where('product_id', $product_id)->get();
             $ticket = Ticket::where('ic', $student->ic)->where('product_id', $product_id)->first();
             
-            if ($student->stud_id == $ticket->ticket_id || $payment->stud_id){
-                return redirect('check-cert/' . $product_id . '/' . $student->stud_id);
-            }
             // Check if ic exist
-            // if($ultimate->membership_id == 'MB001'){
+            if($ultimate->membership_id == 'MB001'){
                 
-            //     return redirect('check-cert/' . $product_id . '/' . $platinum->stud_id);
+                return redirect('check-cert/' . $product_id . '/' . $platinum->stud_id);
 
-            // }else if($platinum->membership_id == 'MB002'){
+            }else if($platinum->membership_id == 'MB002'){
 
-            //     return redirect('check-cert/' . $product_id . '/' . $ultimate->stud_id);
+                return redirect('check-cert/' . $product_id . '/' . $ultimate->stud_id);
 
-            // }else if($check_payment->isEmpty()){
+            }else if($check_payment->isEmpty()){
 
-            //     return view('certificate.not_found');
+                return view('certificate.not_found');
 
-            // }else{
+            }else{
 
-            //     if ($student->stud_id == $ticket->ticket_id || $payment->stud_id){
-            //         return redirect('check-cert/' . $product_id . '/' . $student->stud_id);
-            //     }
+                if ($student->stud_id == $payment->stud_id || $ticket->ticket_id){
+                    return redirect('check-cert/' . $product_id . '/' . $student->stud_id);
+                }
 
-            // }
+            }
             
         }else{
 
