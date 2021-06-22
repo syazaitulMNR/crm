ca4ff0120de60d578b2588cf417593189e840c4d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-22 14:59:14 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -58,7 +58,7 @@
             <p>Date : <b>{{ $date_today }}</b> &nbsp;&nbsp; Time : <b>{{ $current_time }}</b> &nbsp;&nbsp; Report Hours : <b>{{ $duration }}</b></p>
             
             <div class="table-responsive">
-            <table class="table text-center">
+            <table class="table table-bordered text-center">
               <thead class="thead">
                 <tr>
                   <th class="text-left">Package</th>
