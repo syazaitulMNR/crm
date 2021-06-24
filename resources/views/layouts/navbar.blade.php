39a44f928b3ffb3b7c07ec7e3c3d9486ee7bbdd5
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 15:39:47 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,7 +8,7 @@
         </a>
         <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
             {{-- left element --}}
-            <ul class="navbar-nav me-4 mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item ">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
