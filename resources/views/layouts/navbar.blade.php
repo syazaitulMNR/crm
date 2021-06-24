568527e5fd2f33e3078405d0644888feba8a33c8
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 16:04:41 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
             {{-- left element --}}
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item ">
-                    <a class="nav-link" aria-current="page" href="/dashboard">Home</a>
+                    <a class="nav-link" aria-current="page" href="/dashboard"><i class="fas fa-home"></i> Home</a>
                 </li>
                 @if(Auth::user()->role_id == 'ROD003' || Auth::user()->role_id == 'ROD004')
                 @else
