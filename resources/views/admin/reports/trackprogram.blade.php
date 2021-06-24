bde4e9f6f276c41d5ca1460d9f9b90227a473bd5
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 17:07:57 +0800

    Update trackprogram.blade.php

--- resources/views/admin/reports/trackprogram.blade.php
+++ resources/views/admin/reports/trackprogram.blade.php
@@ -6,16 +6,14 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
 
-<div class="row py-4">
-  <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">   
+<div class="col-md-12">    
      
     <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
       <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <b>Customer</b>
     </div>
    
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <div class="flex-md-nowrap pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Customer</h1>
     </div> 
 
@@ -59,58 +57,5 @@
         </table>     
 
       </div>
-                
-      {{-- <div class="col-md-3">
-        <div class="card mb-4 text-center">
-          <div class="card-body pt-4">
-            <i class="fas fa-user-tie fa-3x" style="color:rgb(69, 91, 139)"></i>
-            <h3 class="pt-4">{{$totalcust}}</h3>
-          </div>
-          <div class="card-footer">
-            <h6>Total Customers</h6>
-          </div>
-        </div>
-
-        <div class="card mb-4 text-center">
-          <div class="card-body pt-4">
-            <i class="fa fa-ticket-alt fa-3x" style="color:rgb(69, 139, 69)"></i>
-            <h3 class="pt-4">{{$totalpay}}</h3>
-          </div>
-          <div class="card-footer">
-            <h6>Total Payments</h6>
-          </div>
-        </div>
-
-      </div> --}}
     </div>
-       
-  </main>
 </div>
-
-<!-- Enable function for search data ------------------------------------->
-<script>
-  function myFunction() 
-  {
-    var input, filter, table, tr, td, i, txtValue;
-
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) 
-    {
-      td = tr[i].getElementsByTagName("td")[2];
-      
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
-  }
-</script>
-@endsection
