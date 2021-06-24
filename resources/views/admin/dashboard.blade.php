c2681e45425af94fd8fa442b177a19b6a4187805
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 16:47:32 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -35,7 +35,7 @@
 @section('content')
 
 <div class="col-md-12">     
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div class="px-4 border-bottom">
         <h1 class="h2">Dashboard</h1>
         {{-- <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p> --}}
       </div>
