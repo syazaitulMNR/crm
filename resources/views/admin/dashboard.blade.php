04a5d348c613f4885af898171e6094170b234723
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 16:34:00 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,7 +33,7 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
+{{-- @include('layouts.sidebar') --}}
 <div class="row py-4">     
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       {{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
