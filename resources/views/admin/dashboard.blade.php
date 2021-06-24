441ebdef4eaa25d627271aaa69fa690a18b21c8b
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 16:35:26 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,9 +33,7 @@
 
 @include('layouts.navbar')
 @section('content')
-{{-- @include('layouts.sidebar') --}}
 <div class="row py-4">     
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       {{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         {{-- <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p>
@@ -152,7 +150,6 @@
 
       </div>
       
-    </main>
   </div>
 </div>
 
