7953f411fce77a0c64ecd157652a56cd9ac6fbb9
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 16:45:40 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,11 +33,12 @@
 
 @include('layouts.navbar')
 @section('content')
+
 <div class="col-md-12">     
-      {{-- <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-        {{-- <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p>
-      </div> --}}
+        {{-- <p class="lead">{{ $greetings }}, {{ Auth::user()->name }}.</p> --}}
+      </div>
 
       @if ($message = Session::get('updateprofile'))
       <div class="alert alert-info alert-block">
