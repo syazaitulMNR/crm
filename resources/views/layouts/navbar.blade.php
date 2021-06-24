85db78b3f1dc7dda1d7626edf5270bd24409e801
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 14:47:49 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">
+        <a class="site-logo-inner" href="#">
             <img src="{{ asset('assets/images/logo.png') }}" class="custom-logo" width="80" alt="">
         </a>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
