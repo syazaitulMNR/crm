26416c99cb199614b169d47ad2f38c51eaca0ce5
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-24 15:08:10 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -20,10 +20,6 @@
                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
-            {{-- <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> --}}
             <ul class="nav navbar-nav justify-content-end">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
@@ -64,61 +60,6 @@
     </div>
 </nav>
 @endif
-
-<nav class="navbar navbar-expand-lg navbar-light sticky-top px-5" style="background-color: #ffffff; padding-top:1%; padding-bottom:1%; box-shadow: 0 0px 30px 0 rgba(0, 0, 0, 0.2);">
-    <a class="site-logo-inner" href="#">
-        <img src="{{ asset('assets/images/logo.png') }}" class="custom-logo" width="80" alt="">
-    </a>
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left:2%;">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/">User <span class="sr-only">(current)</span></a>
-          </li>
-      </ul>
-    </div>
-
-    <!-- Right Side Of Navbar -->
-    <ul class="nav navbar-nav justify-content-end">
-        <!-- Authentication Links -->
-        @if (Auth::guest())
-            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-            <!--<li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>-->
-        @else
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-                    <i class="fas fa-user"></i> <span class="caret"></span>
-                </a>
-
-                <ul class="dropdown-menu dropdown-menu-right" role="menu">  
-                    <li class="nav-item">
-                      <a class="dropdown-item" href="/dashboard"><i class="fas fa-home pr-3"></i> Dashboard</a>
-                    </li>
-                    {{-- <li class="nav-item">
-                        {{-- <a class="dropdown-item" href="/manageprofile"><i class="fas fa-address-card pr-3"></i> Manage Profile</a> 
-                        <!-- Button trigger modal -->
-                        <a type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#passwordModal"><i class="fas fa-address-card pr-3"></i> Change Password</a>
-                    </li> --}}
-                    <div class="dropdown-divider"></div>
-                    <li class="nav-item">
-                        <a class="dropdown-item" href="/logout"
-                            onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                           <i class="fas fa-sign-out-alt pr-3"></i> Logout
-                        </a>
-
-                        <form id="logout-form" action="/logout" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                </ul>
-            </li>
-        @endif
-    </ul>
-</nav>
 
 <!-- Modal -->
 {{-- <div class="modal fade" id="passwordModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
