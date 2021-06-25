b024fd42a33e0d959297e0f54fda516c73e07dfc
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-25 22:59:43 +0800

    Update trackprogram.blade.php

--- resources/views/admin/reports/trackprogram.blade.php
+++ resources/views/admin/reports/trackprogram.blade.php
@@ -8,6 +8,24 @@
 @section('content')
 
 <div class="col-md-12">    
+  <div class="card-header pt-3" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
+    <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <b>Customer</b>
+  </div>
+  
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Customer</h1>
+    <p class="lead">{{ $greetings }}</p>
+  </div>
+
+  <div class="row">
+   
+  </div>
+      
+</div>
+
+
+
+{{-- <div class="col-md-12">    
      
   <div class="card-header pt-3" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
     <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <b>Customer</b>
@@ -58,4 +76,4 @@
 
     </div>
   </div>
-</div>
+</div> --}}
