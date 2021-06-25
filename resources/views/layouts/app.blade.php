e8c65152135ddecf27b1bb25af23401a93ef1e1c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-25 23:41:05 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,7 +109,7 @@
     </style>
   </head>
   <body class="bg-light">  
-    <div class='container-fluid px-4 py-4'>
+    <div class='container-fluid px-3 py-4'>
       @yield('content')
     </div>
   </body>
