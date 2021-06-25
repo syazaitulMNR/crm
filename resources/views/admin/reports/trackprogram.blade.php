88b94b3bddc44a7db1c74e28855992e5bf4c3c1f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-25 23:00:03 +0800

    Update trackprogram.blade.php

--- resources/views/admin/reports/trackprogram.blade.php
+++ resources/views/admin/reports/trackprogram.blade.php
@@ -14,7 +14,6 @@
   
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Customer</h1>
-    <p class="lead">{{ $greetings }}</p>
   </div>
 
   <div class="row">
