7a85ac8c0eefb5ccc72a777f74fc197c4920da09
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-26 00:12:04 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -6,7 +6,7 @@
 
 @include('layouts.navbar')
 @section('content')
-<div class="row py-4">     
+<div class="col-md-12 pt-3">     
     <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
       <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> 
       / <a href="/trackprogram">Customer</a> / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"> {{ $product->name }} </a> 
