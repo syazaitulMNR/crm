0f4dd1425bda9cb606c96641879a3450d2fc2365
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-27 11:32:39 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -137,25 +137,25 @@
   <h3 class="border-bottom pb-3">Total Collection</h3>
 
   <div class="row pt-2">
-    <div class="col-md-3 pb-2">
+    <div class="col-md-3 pb-4">
       <div class="card border-0 shadow text-center" style="height: 117px">
         <h6 class="pt-4">{{ $package1->name }}</h6>
         <b class="display-6 pb-3">RM {{ number_format($collection1) }}</b>
       </div>
     </div>
-    <div class="col-md-3 pb-2">
+    <div class="col-md-3 pb-4">
       <div class="card border-0 shadow text-center" style="height: 117px">
         <h6 class="pt-4">{{ $package2->name }}</h6>
         <b class="display-6 pb-3">RM {{ number_format($collection2) }}</b>
       </div>
     </div>
-    <div class="col-md-3 pb-2">
+    <div class="col-md-3 pb-4">
       <div class="card border-0 shadow text-center" style="height: 117px">
         <h6 class="pt-4">{{ $package3->name }}</h6>
         <b class="display-6 pb-3">RM {{ number_format($collection3) }}</b>
       </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 pb-4">
       <div class="card border-0 gradient-2 shadow text-center" style="height: 117px">
         <h6 class="pt-4">Total Collection</h6>
         <b class="display-6 pb-3">RM {{ number_format($collection1 + $collection2 + $collection3) }}</b>
