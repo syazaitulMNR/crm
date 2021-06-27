81477ff4c5794259485aa7598eaf05ef7262ca60
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-27 12:15:13 +0800

    Update viewproduct.blade.php

--- resources/views/admin/viewproduct.blade.php
+++ resources/views/admin/viewproduct.blade.php
@@ -6,106 +6,107 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
-<div class="row py-4">     
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-      
-      <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-        <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <b>Event</b>
-      </div>
 
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Event</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group mr-2">
-            <a href="addproduct" type="button" class="btn btn-sm btn-outline-secondary"><i class="fas fa-plus"></i> Add New Event</a>
-          </div>
-        </div>
-      </div>
+<div class="col-md-12 pt-3">     
       
-      @if ($message = Session::get('success'))
-      <div class="alert alert-success alert-block">
-          <button type="button" class="close" data-bs-dismiss="alert">×</button>	
-          <strong>{{ $message }}</strong>
-      </div>
-      @endif
-      
-      @if ($message = Session::get('updatesuccess'))
-      <div class="alert alert-info alert-block">
-          <button type="button" class="close" data-bs-dismiss="alert">×</button>	
-          <strong>{{ $message }}</strong>
-      </div>
-      @endif
+  <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
+    <a href="/dashboard"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <b>Event</b>
+  </div>
 
-      @if ($message = Session::get('delete'))
-      <div class="alert alert-danger alert-block">
-          <button type="button" class="close" data-bs-dismiss="alert">×</button>	
-          <strong>{{ $message }}</strong>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Event</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group mr-2">
+        <a href="addproduct" type="button" class="btn btn-sm btn-outline-secondary"><i class="fas fa-plus"></i> Add New Event</a>
       </div>
-      @endif
+    </div>
+  </div>
+  
+  @if ($message = Session::get('success'))
+  <div class="alert alert-success alert-block">
+      <button type="button" class="close" data-bs-dismiss="alert">×</button>	
+      <strong>{{ $message }}</strong>
+  </div>
+  @endif
+  
+  @if ($message = Session::get('updatesuccess'))
+  <div class="alert alert-info alert-block">
+      <button type="button" class="close" data-bs-dismiss="alert">×</button>	
+      <strong>{{ $message }}</strong>
+  </div>
+  @endif
 
-      <div class="float-right pt-3">{{$product->links()}}</div>
-      @if(count($product) > 0)
-      <table class="table table-hover">
-        <thead>
+  @if ($message = Session::get('delete'))
+  <div class="alert alert-danger alert-block">
+      <button type="button" class="close" data-bs-dismiss="alert">×</button>	
+      <strong>{{ $message }}</strong>
+  </div>
+  @endif
+
+  <div class="float-right pt-3">{{$product->links()}}</div>
+  @if(count($product) > 0)
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Event Name</th>
+          <th scope="col">Date</th>
+          {{-- <th scope="col">Time</th> --}}
+          <th scope="col">Offer Type</th>
+          <th scope="col"><i class="fas fa-cogs"></i></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($product as $key => $products)    
+        @foreach ($offers as $offer)     
+        @if ($products->offer_id == $offer->offer_id)               
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Event Name</th>
-            <th scope="col">Date</th>
-            {{-- <th scope="col">Time</th> --}}
-            <th scope="col">Offer Type</th>
-            <th scope="col"><i class="fas fa-cogs"></i></th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($product as $key => $products)    
-          @foreach ($offers as $offer)     
-          @if ($products->offer_id == $offer->offer_id)               
-            <tr>
-                <td>{{ $product->firstItem() + $key  }}</td>
-                <td>
-                  {{ $products->name  }} 
-                  @if ($products->status == 'active')
-                      <span class="badge rounded-pill bg-success"> &nbsp;On Going&nbsp; </span>
-                  @else
-                  @endif
-                </td>
-                <td>{{ date('d/m/Y', strtotime($products->date_from)) }} - {{ date('d/m/Y', strtotime($products->date_to)) }}</td>
-                {{-- <td>{{ date('h:i a', strtotime($products->time_from)) }} - {{ date('h:i a', strtotime($products->time_to)) }}</td> --}}
-                <td>{{ $offer->name }}</td>
+              <td>{{ $product->firstItem() + $key  }}</td>
               <td>
-                <a class="btn btn-dark" href="{{ url('package') }}/{{ $products->product_id }}"><i class="fas fa-eye"></i></a>
-                <a class="btn btn-outline-primary" href="{{ url('edit') }}/{{ $products->product_id }}"><i class="fas fa-edit"></i></a>
-                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $products->product_id }}"><i class="fas fa-trash-alt"></i></button>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal{{ $products->product_id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                        Are you sure you want to delete this event ?
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <a class="btn btn-danger" href="{{ url('delete') }}/{{ $products->product_id }}">Delete</a>
-                      </div>
+                {{ $products->name  }} 
+                @if ($products->status == 'active')
+                    <span class="badge rounded-pill bg-success"> &nbsp;On Going&nbsp; </span>
+                @else
+                @endif
+              </td>
+              <td>{{ date('d/m/Y', strtotime($products->date_from)) }} - {{ date('d/m/Y', strtotime($products->date_to)) }}</td>
+              {{-- <td>{{ date('h:i a', strtotime($products->time_from)) }} - {{ date('h:i a', strtotime($products->time_to)) }}</td> --}}
+              <td>{{ $offer->name }}</td>
+            <td>
+              <a class="btn btn-dark" href="{{ url('package') }}/{{ $products->product_id }}"><i class="fas fa-eye"></i></a>
+              <a class="btn btn-outline-primary" href="{{ url('edit') }}/{{ $products->product_id }}"><i class="fas fa-edit"></i></a>
+              <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal{{ $products->product_id }}"><i class="fas fa-trash-alt"></i></button>
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal{{ $products->product_id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      Are you sure you want to delete this event ?
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <a class="btn btn-danger" href="{{ url('delete') }}/{{ $products->product_id }}">Delete</a>
                     </div>
                   </div>
                 </div>
-              </td>
-            </tr>   
-          @endif
-          @endforeach 
-          @endforeach
-        </tbody>
-      </table>
-      @else
-        <p>There are no event to display.</p>
-      @endif
-    </main>
+              </div>
+            </td>
+          </tr>   
+        @endif
+        @endforeach 
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  @else
+    <p>There are no event to display.</p>
+  @endif
+    
   </div>
 </div>
 @endsection
