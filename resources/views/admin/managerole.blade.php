126ffebfe40e3420e3fbeb33314d3f170e4b7886
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-27 14:20:04 +0800

    Update managerole.blade.php

--- resources/views/admin/managerole.blade.php
+++ resources/views/admin/managerole.blade.php
@@ -6,18 +6,16 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
-<div class="row py-4">    
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
 
-        <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
+<div class="col-md-12 pt-3">    
+
+        <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
             <a href="/manageuser"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <a href="/manageuser">Manage User</a> / <b>Manage Role</b>
         </div>
       
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Manage Role</h1>
-      </div>
-      
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Manage Role</h1>
+        </div>
       
         @if ($message = Session::get('rolesuccess'))
         <div class="alert alert-success alert-block">
@@ -40,42 +38,40 @@
             </div>
         @endif
 
-        <form action="{{ url('addrole') }}" method="POST"> 
-        @csrf
-            <div class="row">                  
-                <div class='col-md-3 pr-0'>
-                    <div class="form-group">
-                        <input name="name" type="text" class="form-control" placeholder="Add Role" required>
-                    </div>
+        <form class="row g-3 px-3" action="{{ url('addrole') }}" method="POST"> 
+        @csrf                
+            <div class='col-md-3 pr-0'>
+                <div class="form-group">
+                    <input name="name" type="text" class="form-control" placeholder="Add Role" required>
                 </div>
-                <!--
-                <div class='col-md-4'>
-                    <div class="form-group">
-                        <label for="name">Description</label>
-                        <textarea name="description" type="text" class="form-control" style="height:1cm" required></textarea>
-                    </div>
-                </div>-->
-                    
-                <div class='col-md-2 pl-0 pt-1'>
-                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal1"><i class="fas fa-plus pr-1"></i></button>
+            </div>
+            <!--
+            <div class='col-md-4'>
+                <div class="form-group">
+                    <label for="name">Description</label>
+                    <textarea name="description" type="text" class="form-control" style="height:1cm" required></textarea>
                 </div>
+            </div>-->
+                
+            <div class='col-md-2 pl-0 pt-1'>
+                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal1"><i class="fas fa-plus pr-1"></i></button>
+            </div>
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add Role Confirmation</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                        Are you sure you want to add this role ?
-                        </div>
-                        <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type='submit' class='btn btn-success'>Proceed</button>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Role Confirmation</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+                    <div class="modal-body">
+                    Are you sure you want to add this role ?
                     </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type='submit' class='btn btn-success'>Proceed</button>
+                </div>
                 </div>
             </div>
         </form>
