fdd980fc7ec957bb9f0d38e6ecd27ec635038283
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-27 09:00:45 +0800

    Update package.blade.php

--- resources/views/admin/blasting_email/package.blade.php
+++ resources/views/admin/blasting_email/package.blade.php
@@ -6,57 +6,56 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
-<div class="row py-4">     
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-        <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-          <a href="/emailblast"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/emailblast">Email Blasting</a> / <b>{{ $product->name }}</b>
-        </div>
-  
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">{{ $product->name }}</h1>
 
-        </div>
-
-        <div class="row">
-          <div class="col-md-12 "> 
-        
-            <!-- Show package in table ----------------------------------------------->
-            @if(count($package) > 0)
-            <table class="table table-hover" id="successTable">
-                <thead>
-                <tr class="header">
-                    <th>#</th>
-                    <th>Package Name</th>
-                    <th class="w-25"><i class="fas fa-cogs"></i></th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($package as $key => $packages)    
-                @if ($product->product_id == $packages->product_id)   
-                <tr>
-                  <td>{{ $package->firstItem() + $key }}</td>
-                  <td>{{ $packages->name  }}</td>
-                  <td>
-                    <a class="btn btn-sm btn-dark" href="{{ url('view-event') }}/{{ $product->product_id }}/{{ $packages->package_id }}"><i class="fas fa-user pr-1"></i> Buyer</a>                   
-                    <a class="btn btn-sm btn-dark" href="{{ url('blast-participant') }}/{{ $product->product_id }}/{{ $packages->package_id }}"><i class="fas fa-users pr-1"></i> Participant</a>
-                  </td>
-                </tr>
-                @endif
-                @endforeach
-                </tbody>
-            </table>  
-            @else
-            <p>There are no package yet.</p>
-            @endif
-            <div class="float-right pt-3">{{$package->links()}}</div>
-            
-          </div>
-          
-        </div>
-        
-    </main>
+<div class="col-md-12 pt-3">     
+    
+  <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
+    <a href="/emailblast"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/emailblast">Email Blasting</a> / <b>{{ $product->name }}</b>
   </div>
+  
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">{{ $product->name }}</h1>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12 "> 
+  
+      <!-- Show package in table ----------------------------------------------->
+      @if(count($package) > 0)
+      <div class="table-responsive">
+        <table class="table table-hover" id="successTable">
+          <thead>
+            <tr class="header">
+              <th>#</th>
+              <th>Package Name</th>
+              <th class="w-25"><i class="fas fa-cogs"></i></th>
+            </tr>
+          </thead>
+          <tbody>
+          @foreach ($package as $key => $packages)    
+          @if ($product->product_id == $packages->product_id)   
+          <tr>
+            <td>{{ $package->firstItem() + $key }}</td>
+            <td>{{ $packages->name  }}</td>
+            <td>
+              <a class="btn btn-sm btn-dark" href="{{ url('view-event') }}/{{ $product->product_id }}/{{ $packages->package_id }}"><i class="fas fa-user pr-1"></i> Buyer</a>                   
+              <a class="btn btn-sm btn-dark" href="{{ url('blast-participant') }}/{{ $product->product_id }}/{{ $packages->package_id }}"><i class="fas fa-users pr-1"></i> Participant</a>
+            </td>
+          </tr>
+          @endif
+          @endforeach
+          </tbody>
+        </table>  
+      </div>
+      @else
+      <p>There are no package yet.</p>
+      @endif
+      <div class="float-right pt-3">{{$package->links()}}</div>
+      
+    </div>
+    
+  </div>
+        
 </div>
 
 
