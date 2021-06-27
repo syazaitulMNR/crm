62330bc7c97f2c7a778f169eb3ec6e98afe391c9
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-27 13:55:19 +0800

    Update updatepackage.blade.php

--- resources/views/admin/updatepackage.blade.php
+++ resources/views/admin/updatepackage.blade.php
@@ -6,77 +6,72 @@
 
 @include('layouts.navbar')
 @section('content')
-@include('layouts.sidebar')
 
-<div class="row py-4">
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+<div class="col-md-12 pt-3">
 
-        <div class="card-header" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-            <a href="{{ url('package')}}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Update Package</b>
-        </div>
+    <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
+        <a href="{{ url('package')}}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">Dashboard</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Update Package</b>
+    </div>
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Update Package</h1>
+    </div>
     
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 class="h2">Update Package</h1>
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-bs-dismiss="alert">×</button>	
+            <strong>{{ $message }}</strong>
         </div>
-        
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success alert-block">
-                <button type="button" class="close" data-bs-dismiss="alert">×</button>	
-                <strong>{{ $message }}</strong>
-            </div>
-        @endif
+    @endif
 
-        <form action="{{ url('updatepack') }}/{{ $package->package_id }}/{{ $product->product_id }}" method="POST" enctype="multipart/form-data"> 
-        @csrf
-            <div class="row pt-3" style="padding-left: 8%">
-                <div class='col-md-8'>
-                    <div class='row'>
-                        <div class='col-md-9'>         
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input name="name" type="text" class="form-control" value="{{ $package->name }}" required>
-                            </div>
-                        </div>
-                
-                        <div class='col-md-3'>
-                            <div class="form-group">
-                                <label for="phone">Price (RM)</label>
-                                <input name="price" type="text" class="form-control" value="{{ $package->price }}" required>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- <div class="col-md-8">                         
+    <form action="{{ url('updatepack') }}/{{ $package->package_id }}/{{ $product->product_id }}" method="POST" enctype="multipart/form-data"> 
+    @csrf
+        <div class='col-md-8'>
+            <div class='row'>
+                <div class='col-md-9'>         
                     <div class="form-group">
-                        <label for="formFile" class="form-label">Package Image</label>
-                        <input class="form-control" type="file" id="formFile" name="package_image">
+                        <label for="name">Name</label>
+                        <input name="name" type="text" class="form-control" value="{{ $package->name }}" required>
                     </div>
-                </div> --}}
-
-                <div class='col-md-8'>
-                    <label for="name">Features</label>
-                    @foreach ($feature as $value)
-                    <div id="inputFormRow">
-                        <div class="input-group mb-3">
-                            <input name="feature[]" type="text" class="form-control" value="{{ $value->name }}" >
-                            <input name="feat_id[]" type="hidden" class="form-control" value="{{ $value->feat_id }}">
-                        </div>
+                </div>
+        
+                <div class='col-md-3'>
+                    <div class="form-group">
+                        <label for="phone">Price (RM)</label>
+                        <input name="price" type="text" class="form-control" value="{{ $package->price }}" required>
                     </div>
-                    @endforeach
-
-                    <div id="newRow"></div>
-                    <button id="addRow" type='button' class='btn'><i class="fas fa-plus pr-1"></i> Add Row</button>
                 </div>
-                    
-                <div class='col-md-8 pt-3'>
-                    <button type='submit' class='btn btn-primary float-right'> Update </button>
-                </div>
-
             </div>
-        </form>
+        </div>
 
-    </main>
+        {{-- <div class="col-md-8">                         
+            <div class="form-group">
+                <label for="formFile" class="form-label">Package Image</label>
+                <input class="form-control" type="file" id="formFile" name="package_image">
+            </div>
+        </div> --}}
+
+        <div class='col-md-8'>
+            <label for="name">Features</label>
+            @foreach ($feature as $value)
+            <div id="inputFormRow">
+                <div class="input-group mb-3">
+                    <input name="feature[]" type="text" class="form-control" value="{{ $value->name }}" >
+                    <input name="feat_id[]" type="hidden" class="form-control" value="{{ $value->feat_id }}">
+                </div>
+            </div>
+            @endforeach
+
+            <div id="newRow"></div>
+            <button id="addRow" type='button' class='btn'><i class="fas fa-plus pr-1"></i> Add Row</button>
+        </div>
+            
+        <div class='col-md-8 pt-3'>
+            <button type='submit' class='btn btn-primary float-right'> Update </button>
+        </div>
+
+    </form>
+
 </div>
 
 <!-- Function to add row --------------------------------------------------->
