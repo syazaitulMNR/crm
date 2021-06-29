4e756568a44c507d82ef2b371886f5d907dd79ab
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-29 15:27:57 +0800

    Update trackpayment.blade.php

--- resources/views/admin/reports/trackpayment.blade.php
+++ resources/views/admin/reports/trackpayment.blade.php
@@ -49,13 +49,11 @@
                             <label class="col-sm-2">IC No.</label>
                             <div class="col-sm-4">
                                 <input type="text" class="form-control" name="ic" value="{{ $student->ic }}">
-                                {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->ic }}</p> --}}
                             </div>
 
                             <label class="col-sm-2">Phone No.</label>
                             <div class="col-sm-4">
                                 <input type="text" class="form-control" name="phoneno" value="{{ $student->phoneno }}">
-                                {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->phoneno }}</p> --}}
                             </div>
                         </div>
                         <div class="mb-3 row">
@@ -63,14 +61,12 @@
                             <div class="col-sm-4">
                                 <input type="text" class="form-control" name="first_name" value="{{ $student->first_name }}" placeholder="First Name">
                                 <input type="text" class="form-control" name="last_name" value="{{ $student->last_name }}" placeholder="Last Name">
-                                {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->first_name }}&nbsp;{{ $student->last_name }}</p> --}}
                             </div>
 
                             <label class="col-sm-2">Email Address</label>
                             <div class="col-sm-4">
                                 
                                 <input type="text" class="form-control" name="email" value="{{ $student->email }}">
-                                {{-- <p>: &nbsp;&nbsp;&nbsp; {{ $student->email }}</p> --}}
                                 
                                 <br>
                                                 
