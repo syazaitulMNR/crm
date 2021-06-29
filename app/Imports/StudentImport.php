82a216f746d342de88afec545fee9b03749f3696
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-29 14:40:50 +0800

    Update StudentImport.php

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -7,14 +7,10 @@ use App\Payment;
 use App\Ticket;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
-// use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-// use Illuminate\Contracts\Queue\ShouldQueue;
 
-// class StudentImport implements ToModel, WithHeadingRow, WithChunkReading, ShouldQueue
-// class StudentImport implements ToModel, WithChunkReading, WithHeadingRow
 class StudentImport implements ToCollection, WithChunkReading, WithHeadingRow
 {
     private $prd_id, $pkd_id;
@@ -27,9 +23,6 @@ class StudentImport implements ToCollection, WithChunkReading, WithHeadingRow
     public function collection(Collection $rows)
     {
         // dump($rows[2]);
-
-        // $stud_id = 'MI' . uniqid();
-        // $payment_id = 'OD' . uniqid();
 
         foreach ($rows as $row) 
         {
@@ -53,17 +46,6 @@ class StudentImport implements ToCollection, WithChunkReading, WithHeadingRow
                     'product_id'    => $this->product,
                     'package_id'    => $this->package,
                 ]);
-
-                // $ticket_id = 'TIK' . uniqid();
-
-                // Ticket::create([
-                //     'ticket_id'     => $ticket_id,
-                //     'ticket_type'   => $row['ticket_type'],
-                //     'ic'            => $row['ic'],
-                //     'product_id'    => $this->product,
-                //     'package_id'    => $this->package,
-                //     'payment_id'    => $payment_id
-                // ]);
 
             }else{
 
@@ -96,47 +78,10 @@ class StudentImport implements ToCollection, WithChunkReading, WithHeadingRow
                     'package_id'    => $this->package,
                 ]);
 
-                // $ticket_id = 'TIK' . uniqid();
-
-                // Ticket::create([
-                //     'ticket_id'     => $ticket_id,
-                //     'ticket_type'   => $row['ticket_type'],
-                //     'ic'            => $row['ic'],
-                //     'product_id'    => $this->product,
-                //     'package_id'    => $this->package,
-                //     'payment_id'    => $payment_id
-                // ]);
             }
         }
         
     }
-
-    // /**
-    // * @param array $row
-    // *
-    // * @return \Illuminate\Database\Eloquent\Model|null
-    // */
-    // public function model(array $row)
-    // {
-    //     // $student = Student::orderBy('id','Desc')->first();
-
-    //     // $auto_inc = $student->id + 1;
-    //     // $stud_id = 'MI' . 0 . 0 . $auto_inc;
-        
-    //     if(Student::where('ic', $row['ic'])->exists()){
-    //     }else{
-    //         return new Student([
-    //             // 'stud_id'    => $stud_id,
-    //             'stud_id'    => $row['stud_id'],
-    //             'first_name' => $row['first_name'],
-    //             'last_name'  => $row['last_name'], 
-    //             'ic'         => $row['ic'],
-    //             'email'      => $row['email'],
-    //             'phoneno'    => $row['phoneno'],                
-    //         ]);
-    //     }
-        
-    // }
 
     public function chunkSize(): int
     {
