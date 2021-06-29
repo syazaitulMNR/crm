26e46b20e3dd774bead0eceacfd38c1c87909352
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-29 12:29:34 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -103,8 +103,6 @@ class ProductController extends Controller
         $product = Product::where('product_id', $id)->first();    
         $check_image = $request->hasFile('cert_image');
         
-        // dd($check_image);
-
         if($check_image == false){
 
             $product->name = $request->prodname;
