02d3be01c3d2687e87169129bebf037df865cafe
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-06-29 09:29:21 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -144,7 +144,7 @@ class AdminController extends Controller
         $totalregister = Payment::where('status','paid')->where('product_id', $product_id)->count();
         $totalpaid = Ticket::where('ticket_type', 'paid')->where('product_id', $product_id)->count();
         $totalfree = Ticket::where('ticket_type', 'free')->where('product_id', $product_id)->count();
-        $totalticket = Payment::where('product_id', $product_id)->where('status','paid')->count();
+        $totalticket = Payment::where('product_id', $product_id)->where('update_count','1')->count();
         $pendingticket = $totalregister - $totalpaid;
 
         // get total collection
