8b5d44d4ac9010965e60d57a6079553bde2f6df9
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-02 21:48:56 +0800

    Update updatepackage.blade.php

--- resources/views/admin/updatepackage.blade.php
+++ resources/views/admin/updatepackage.blade.php
@@ -10,7 +10,7 @@
 <div class="col-md-12 pt-3">
 
     <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-        <a href="{{ url('package')}}/{{ $product->product_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="dashboard">...</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Update Package</b>
+        <a href="{{ url('package')}}/{{ $product->product_id }}"><i class="bi bi-arrow-left"></i></a> &nbsp; <a href="dashboard">...</a> / <a href="/product">Event</a> / <a href="{{ url('package')}}/{{ $product->product_id }}">Package</a> / <b>Update Package</b>
     </div>
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -67,7 +67,7 @@
         </div>
             
         <div class='col-md-8 pt-3'>
-            <button type='submit' class='btn btn-primary float-right'> Update </button>
+            <button type='submit' class='btn btn-primary float-right'> <i class="bi bi-save pr-2"></i>Update </button>
         </div>
 
     </form>
