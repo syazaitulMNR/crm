504eba59ee9b6093d5e630d551c0c51ba330fe76
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-02 15:37:15 +0800

    Update trackpayment.blade.php

--- resources/views/admin/reports/trackpayment.blade.php
+++ resources/views/admin/reports/trackpayment.blade.php
@@ -9,8 +9,8 @@
 <div class="col-md-12 pt-3"> 
         
     <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-        <a href="{{ url('view/buyer') }}/{{ $product->product_id }}/{{ $package->package_id }}"><i class="fas fa-arrow-left"></i></a> &nbsp; 
-        <a href="{{ url('trackpackage') }}/{{ $product->product_id }}">...</a> 
+        <a href="{{ url('view/buyer') }}/{{ $product->product_id }}/{{ $package->package_id }}"><i class="bi bi-arrow-left"></i></a> &nbsp; <a href="/trackprogram">...</a>
+        / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}">{{ $product->name }}</a> 
         / <a href="{{ url('view/buyer') }}/{{ $product->product_id }}/{{ $package->package_id }}">{{ $package->name }}</a> / <b>{{ $student->first_name }}</b>
     </div>
             
