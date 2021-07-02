6a98ce62a3ee3e387b6c9a83d73f5b5b63e2933c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-02 23:42:31 +0800

    Update managerole.blade.php

--- resources/views/admin/managerole.blade.php
+++ resources/views/admin/managerole.blade.php
@@ -71,7 +71,7 @@
                     </div>
                     <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type='submit' class='btn btn-success'>Proceed</button>
+                    <button type='submit' class='btn btn-primary'>Submit</button>
                 </div>
                 </div>
             </div>
