fbaca7d4a73e8204683f9679744716709754ae9b
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-02 23:28:34 +0800

    Update viewoffer.blade.php

--- resources/views/admin/viewoffer.blade.php
+++ resources/views/admin/viewoffer.blade.php
@@ -41,7 +41,7 @@
                         </div>
                                             
                         <div class='col-md-12 text-right px-4'>
-                            <button type='submit' class='btn btn-success'> <i class="bi bi-save pr-2"></i>Save </button>
+                            <button type='submit' class='btn btn-submit'> <i class="bi bi-save pr-2"></i>Submit </button>
                         </div>
                     </form>
                 </div>
