9658256a45a97ef9e3d099fdb185228ddeb6602e
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-02 23:39:02 +0800

    Update updateuser.blade.php

--- resources/views/admin/updateuser.blade.php
+++ resources/views/admin/updateuser.blade.php
@@ -11,7 +11,7 @@
 <div class="col-md-12 pt-3">     
 
     <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
-        <a href="/manageuser"><i class="fas fa-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/manageuser">Manage User</a> / <b>User Information</b>
+        <a href="/manageuser"><i class="bi bi-arrow-left"></i></a> &nbsp; <a href="/dashboard">Dashboard</a> / <a href="/manageuser">Manage User</a> / <b>User Information</b>
     </div>
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -92,7 +92,7 @@
 
             <div class='col-md-8 pt-3'>
                 <button type="submit" class="btn btn-primary float-right">
-                    Update
+                    <i class="bi bi-save pr-2"></i>Save Changes
                 </button>
             </div>
         
