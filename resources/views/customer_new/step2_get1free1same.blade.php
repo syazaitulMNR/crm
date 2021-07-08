92def67ffd3d4d3551282aa94b7becc258f61b61
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-08 13:11:03 +0800

    Update step2_get1free1same.blade.php

--- resources/views/customer_new/step2_get1free1same.blade.php
+++ resources/views/customer_new/step2_get1free1same.blade.php
@@ -5,79 +5,75 @@ Pendaftaran Pembeli
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 px-3 py-5 text-center">
-            <img src="/assets/images/logo.png" style="max-width:200px">
-            <h1 class="display-4 text-dark px-4 pt-3">{{ $product->name }}</h1>
-        </div>
+<div class="row">
+    <div class="col-md-12 px-2 py-5 text-center">
+        <img src="/assets/images/logo.png" style="max-width:150px">
+        <h1 class="display-5 text-dark px-3 pt-4">{{ $product->name }}</h1>
+    </div>
 
-        <div class="col-md-12 d-flex justify-content-center pb-5">
-            <form action="{{ url('store2') }}/{{ $product->product_id }}/{{ $package->package_id }}" method="POST">
-                @csrf
-                <div class="card w-100 shadow">
-                    <div class="card-header bg-dark text-white">Langkah 2/5: Maklumat Tiket</div>
-  
-                    <div class="card-body">
-  
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul class="px-3">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-  
-                        <div class="form-group row">
-                            <input type="hidden" value="{{ $payment_id ?? ''}}" class="form-control" name="payment_id" readonly/>
-                            <input type="hidden" value="{{ $product->product_id }}" class="form-control" name="product_id" readonly/>
-                            <input type="hidden" value="{{ $package->package_id }}" class="form-control" name="package_id" readonly/>
-                            <input type="hidden" value="{{ $student->stud_id }}" class="form-control" name="stud_id" readonly/>
-                            <input type="hidden" value="{{ $product->offer_id }}" class="form-control" name="offer_id" readonly/>
+    <div class="col-md-6 offset-md-3 pb-5">
+        <form action="{{ url('store2') }}/{{ $product->product_id }}/{{ $package->package_id }}" method="POST">
+            @csrf
+            <div class="card px-4 py-4 shadow">
+                <div class="bg-dark text-white px-2 py-2">Langkah 2/5: Maklumat Tiket</div>
 
-                            <div class="col-md-6 pb-2">
-                                <label for="package">Pakej:</label>
-                                <input type="text"  value="{{ $package->name }}" class="form-control" readonly/>
+                <div class="card-body">
+
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul class="px-3">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
                             </div>
-                            <div class="col-md-6 pb-2">
-                                <label for="price">Harga:</label>
-                                <input type="text" value="{{ $package->price }}" class="form-control" readonly/>
-                                <input type="hidden" id="price" name="price" value="{{ $package->price }}" readonly>
-                                <input type="hidden" name="pay_price" value="{{ $package->price }}" readonly>
-                            </div>
-                            <div class="col-md-6 pb-2">
-                                <label for="quantity">Kuantiti:</label>
-                                <select id="quantity" name="quantity" onchange="calculateAmount(this.value)" value="{{ $payment->quantity ?? '' }}" class="form-control" required>
-                                    <option value="" disabled selected>-- Tiket --</option>
-                                    {{-- <option value="1">1 (Free 1 General)</option> --}}
-                                    <option value="1">1 (Free 1 {{ $package->name }})</option>
-                                </select>
-                            </div>
-                            <div class="col-md-6 pb-2">
-                                <label for="totalprice">Jumlah Harga (RM)</label><br>
-                                <h3><input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none; outline-width: 0;" readonly></h3>
-                            </div>
+                        @endif
+
+                    <div class="form-group row">
+                        <input type="hidden" value="{{ $payment_id ?? ''}}" class="form-control" name="payment_id" readonly/>
+                        <input type="hidden" value="{{ $product->product_id }}" class="form-control" name="product_id" readonly/>
+                        <input type="hidden" value="{{ $package->package_id }}" class="form-control" name="package_id" readonly/>
+                        <input type="hidden" value="{{ $student->stud_id }}" class="form-control" name="stud_id" readonly/>
+                        <input type="hidden" value="{{ $product->offer_id }}" class="form-control" name="offer_id" readonly/>
+
+                        <div class="col-md-6 pb-2">
+                            <label for="package">Pakej:</label>
+                            <input type="text"  value="{{ $package->name }}" class="form-control" readonly/>
                         </div>
-  
+                        <div class="col-md-6 pb-2">
+                            <label for="price">Harga:</label>
+                            <input type="text" value="{{ $package->price }}" class="form-control" readonly/>
+                            <input type="hidden" id="price" name="price" value="{{ $package->price }}" readonly>
+                            <input type="hidden" name="pay_price" value="{{ $package->price }}" readonly>
+                        </div>
+                        <div class="col-md-6 pb-2">
+                            <label for="quantity">Kuantiti:</label>
+                            <select id="quantity" name="quantity" onchange="calculateAmount(this.value)" value="{{ $payment->quantity ?? '' }}" class="form-control" required>
+                                <option value="" disabled selected>-- Tiket --</option>
+                                {{-- <option value="1">1 (Free 1 General)</option> --}}
+                                <option value="1">1 (Free 1 {{ $package->name }})</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 pb-2">
+                            <label for="totalprice">Jumlah Harga (RM)</label><br>
+                            <h3><input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none; outline-width: 0;" readonly></h3>
+                        </div>
                     </div>
-                    
-                    <div class="card-footer">
-                        <div class="col-md-12">
-                            <div class="pull-left">
-                                <a href="{{ url('maklumat-pembeli') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->ic }}" class="btn btn-circle btn-lg btn-outline-dark"><i class="fas fa-arrow-left" style="padding-top:35%"></i></a>
-                            </div>
-                            <div class="pull-right">
-                                <button type="submit" class="btn btn-circle btn-lg btn-dark"><i class="fas fa-arrow-right py-1"></i></button>
-                            </div>
-                        </div>
+
+                </div>
+                
+                <div class="col-md-12">
+                    <div class="pull-left">
+                        <a href="{{ url('maklumat-pembeli') }}/{{ $product->product_id }}/{{ $package->package_id }}/{{ $student->ic }}" class="btn btn-circle btn-lg btn-outline-dark"><i class="fas fa-arrow-left" style="padding-top:35%"></i></a>
+                    </div>
+                    <div class="pull-right">
+                        <button type="submit" class="btn btn-circle btn-lg btn-dark"><i class="fas fa-arrow-right py-1"></i></button>
                     </div>
                 </div>
-            </form>
-        </div>
-
+            </div>
+        </form>
     </div>
+
 </div>
 
 @endsection
