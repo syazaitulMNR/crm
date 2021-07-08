ad619779ab7c4c20ed32572b8140e3296a6a1983
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-08 13:06:04 +0800

    Update step2_nooffer.blade.php

--- resources/views/customer_new/step2_nooffer.blade.php
+++ resources/views/customer_new/step2_nooffer.blade.php
@@ -64,7 +64,7 @@ Pendaftaran Pembeli
                         </div>
                         <div class="col-md-6 pb-2">
                             <label for="totalprice">Jumlah Harga (RM)</label><br>
-                            <h3><input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none;" readonly></h3>
+                            <h3><input type="text" id="totalprice" name="totalprice" value="{{ $payment->totalprice ?? '' }}" style="border: none; background-color: transparent;" readonly></h3>
                         </div>
                     </div>
 
