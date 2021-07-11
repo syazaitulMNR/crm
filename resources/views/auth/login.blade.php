c63562319cf07106eaabb89067f8365e10229421
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-12 07:31:54 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,55 +13,57 @@
 
     <div class="col-md-4 offset-md-4">
         
-        <div class="card text-center shadow">
+        <div class="card px-4 py-4 text-center shadow">
             
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-key"></i></span>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-key"></i></span>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-                <div class="form-group row text-center">
-                    <div class="col-md-12">
-                        <div class="checkbox">
-                            <label><input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/> {{ __('Remember Me') }}</label>
+                    <div class="form-group row text-center">
+                        <div class="col-md-12">
+                            <div class="checkbox">
+                                <label><input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/> {{ __('Remember Me') }}</label>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="form-group row text-center">
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-sm btn-dark">
-                            <i class="bi bi-box-arrow-in-right pr-1"></i> {{ __('Login') }}
-                        </button>
+                    <div class="form-group row text-center">
+                        <div class="col-md-12">
+                            <button type="submit" class="btn btn-sm btn-dark">
+                                <i class="bi bi-box-arrow-in-right pr-1"></i> {{ __('Login') }}
+                            </button>
+                        </div>
+
+                        {{-- <div class="col-md-12">
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                        </div> --}}
                     </div>
-
-                    {{-- <div class="col-md-12">
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </div> --}}
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
 
     </div>
