47b3114a03f9e7d85b758d1f692fc8b4c6bfd75f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-16 09:38:41 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -137,7 +137,7 @@ class AdminController extends Controller
         $totalpackage3 = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package3->package_id)->whereBetween('created_at', [ date('2021-07-14 00:00:00') , date('2021-07-15 23:59:59') ])->count();
 
         // get the grand total
-        $totalregister = Payment::where('status','paid')->where('product_id', $product_id)->count();
+        $totalregister = Payment::where('status','paid')->where('product_id', $product_id)->whereBetween('created_at', [ date('2021-07-14 00:00:00') , date('2021-07-15 23:59:59') ])->count();
         $totalpaid = Ticket::where('ticket_type', 'paid')->where('product_id', $product_id)->count();
         $totalfree = Ticket::where('ticket_type', 'free')->where('product_id', $product_id)->count();
         $totalticket = Ticket::where('product_id', $product_id)->whereBetween('created_at', [ date('2021-07-14 00:00:00') , date('2021-07-15 23:59:59') ])->count();
