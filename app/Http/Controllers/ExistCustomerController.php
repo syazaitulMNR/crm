823691d3c0b1f0a56e5d86e0633770f3bcf89fde
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-17 15:46:41 +0800

    Update ExistCustomerController.php

--- app/Http/Controllers/ExistCustomerController.php
+++ app/Http/Controllers/ExistCustomerController.php
@@ -53,36 +53,36 @@ class ExistCustomerController extends Controller
         $student = Student::where('stud_id', $stud_id)->first();
         $product = Product::where('product_id',$product_id)->first();
         $package = Package::where('package_id', $package_id)->first();
-        $package_name = Package::where('package_id', $package_id)->get();
+        $package_name = Package::where('product_id', $product_id)->get();
         $stud = $request->session()->get('student');
         $payment = $request->session()->get('payment');
 
-        
+        dd($package_name[0]->name);
 
-        //generate id
-        $payment_id = 'OD'.uniqid();
+        // //generate id
+        // $payment_id = 'OD'.uniqid();
 
-        if($product->offer_id == 'OFF001') {
+        // if($product->offer_id == 'OFF001') {
 
-            //for no offer ticket
-            return view('customer_exist.step2_nooffer',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
+        //     //for no offer ticket
+        //     return view('customer_exist.step2_nooffer',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
 
-        } else if($product->offer_id == 'OFF002') {
+        // } else if($product->offer_id == 'OFF002') {
 
             
-            //for Buy 1 Get 1 (Same Ticket)
-            return view('customer_exist.step2_get1free1same',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
+        //     //for Buy 1 Get 1 (Same Ticket)
+        //     return view('customer_exist.step2_get1free1same',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
 
-        } else if($product->offer_id == 'OFF003') {
+        // } else if($product->offer_id == 'OFF003') {
 
-            //for Bulk Ticket
-            return view('customer_exist.step2_bulkticket',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
+        //     //for Bulk Ticket
+        //     return view('customer_exist.step2_bulkticket',compact('student', 'payment', 'product', 'package', 'payment_id', 'package_name'));
 
-        } else {
+        // } else {
 
-            echo 'No Such Offer';
+        //     echo 'No Such Offer';
 
-        }
+        // }
   
     }
 
