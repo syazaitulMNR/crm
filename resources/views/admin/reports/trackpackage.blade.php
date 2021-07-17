32b26e6f830dc2d8f3cc18ceaf6b9c79ada52d0f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-17 17:04:19 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -30,7 +30,9 @@ Sales Report
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    ...
+                    <ul>
+                      <li>All Buyer</li>
+                    </ul>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
