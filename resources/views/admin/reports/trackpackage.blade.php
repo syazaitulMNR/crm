d4c876d53ab5c87630facb509f3ab77a2a0bb48b
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-17 17:13:00 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -16,42 +16,47 @@ Sales Report
 
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#BuyerModal">
+            <!-- Button trigger modal exportProgram -->
+            <button type="button" class="btn btn-sm btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exportProgram">
               <i class="bi bi-download pr-2"></i>Export Buyer
             </button>
-
             <!-- Modal -->
-            <div class="modal fade" id="BuyerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
+            <div class="modal fade" id="exportProgram" tabindex="-1" role="dialog" aria-labelledby="exportProgramLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
-                  <div class="modal-header">
+                  <div class="modal-header border-bottom-0">
                     <h5 class="modal-title" id="exampleModalLabel">Export Buyer</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
                   </div>
-                  <div class="modal-body">
-                    <p>Please choose data that you want to export :</p>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                        Success Payment Only
-                      </label>
-                    </div><div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                        Updated Participant Only
-                      </label>
-                    </div><div class="form-check">
-                      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                      <label class="form-check-label" for="exampleRadios2">
-                        All Buyer
-                      </label>
+                  <form action="" method="POST"> 
+                  @csrf
+                  
+                    <div class='col-md-12 px-4'>
+                      <p>Please choose data that you want to export :</p>
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                        <label class="form-check-label" for="exampleRadios2">
+                          Success Payment Only
+                        </label>
+                      </div><div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                        <label class="form-check-label" for="exampleRadios2">
+                          Updated Participant Only
+                        </label>
+                      </div><div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                        <label class="form-check-label" for="exampleRadios2">
+                          All Buyer
+                        </label>
+                      </div>
                     </div>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                  </div>
+
+                    <div class='col-md-12 text-right px-4'>
+                        <button type='submit' class='btn btn-success'> <i class="bi bi-save pr-2"></i>Save</button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
