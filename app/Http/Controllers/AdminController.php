6d0603c0d3213d8f8204aaf856209d01698058b3
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-18 18:31:41 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -115,6 +115,7 @@ class AdminController extends Controller
 
         // get package id
         $package = Package::where('product_id', $product_id)->get();
+        dd($package[0]->name);
         // $package_id = Package::where('product_id', $product_id)->pluck('package_id');
         $package1 = Package::where('package_id', 'PKD0018')->first();
         $package2 = Package::where('package_id', 'PKD0019')->first();
@@ -153,7 +154,7 @@ class AdminController extends Controller
         $collection2 = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package2->package_id)->sum('totalprice');
         $collection3 = Payment::where('status','paid')->where('product_id', $product_id)->where('package_id', $package3->package_id)->sum('totalprice');
         
-        return view('admin.dashboard', compact('product', 'package', 'package1', 'package2', 'package3', 'date_today', 'current_time', 'from', 'to', 'duration', 'greetings', 'register', 'paid', 'free', 'totalregister', 'totalpaid', 'totalfree', 'totalticket', 'registration1', 'paidticket1', 'freeticket1', 'totalpackage1', 'registration2', 'paidticket2', 'freeticket2', 'totalpackage2', 'registration3', 'paidticket3', 'freeticket3', 'totalpackage3', 'pendingticket', 'collection1', 'collection2', 'collection3'));
+        // return view('admin.dashboard', compact('product', 'package', 'package1', 'package2', 'package3', 'date_today', 'current_time', 'from', 'to', 'duration', 'greetings', 'register', 'paid', 'free', 'totalregister', 'totalpaid', 'totalfree', 'totalticket', 'registration1', 'paidticket1', 'freeticket1', 'totalpackage1', 'registration2', 'paidticket2', 'freeticket2', 'totalpackage2', 'registration3', 'paidticket3', 'freeticket3', 'totalpackage3', 'pendingticket', 'collection1', 'collection2', 'collection3'));
     }
 
     /*-- Manage User --------------------------------------------------------*/
