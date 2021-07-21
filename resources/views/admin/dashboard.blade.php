ccfbb6223ce79e0bcf8ecca0693941405fafb379
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-07-21 14:56:54 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
 
 <div class="col-md-12">     
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Dashboard</h1>
+    <h1 class="h2">Dashboard Je</h1>
     <p class="lead">{{ $greetings }}</p>
   </div>
 
