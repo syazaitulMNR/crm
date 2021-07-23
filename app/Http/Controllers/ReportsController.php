3c3261958e6775181cfc7da388362a7a7e5ab065
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-23 13:18:36 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -58,7 +58,7 @@ class ReportsController extends Controller
         $counter = Student::count();
         $totalsuccess = Payment::where('status','paid')->where('product_id', $product_id)->count();
         $totalcancel = Payment::where('status','due')->where('product_id', $product_id)->count();
-        $paidticket = Payment::where('product_id', $product_id)->where('status', 'paid')->where('update_count', 1)->count();
+        $paidticket = Ticket::where('ticket_type', 'paid')->where('product_id', $product_id)->count();
         $freeticket = Ticket::where('ticket_type', 'free')->where('product_id', $product_id)->count();
         
         return view('admin.reports.trackpackage', compact('product', 'package', 'payment', 'student', 'counter', 'totalsuccess', 'totalcancel', 'paidticket', 'freeticket'));
