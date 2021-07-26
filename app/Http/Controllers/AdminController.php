0a2329bcba581b41c00fa4de63294e873fa80e31
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-26 15:17:24 +0800

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -130,7 +130,7 @@ class AdminController extends Controller
             
         }
         
-        echo '{{$package}} <br> System is currently under maintenance. Thank you :)';
+        echo '$i <br> System is currently under maintenance. Thank you :)';
 
         // $package1 = $package[0]->name;
         // $package2 = $package[1]->name;
