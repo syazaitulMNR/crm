c7cef6ef3c5c7445e3f899a42b6a25dce89acee5
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-07-26 10:43:53 +0800

    Update trackticket.blade.php

--- resources/views/admin/reports/trackticket.blade.php
+++ resources/views/admin/reports/trackticket.blade.php
@@ -137,7 +137,10 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body text-start">
-                            Are you sure you want to delete this payment ?
+                                <p>This action will remove the details from the table :</p>
+                                <ul>
+                                  <li>Ticket</li>
+                                </ul>
                             </div>
                             <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
