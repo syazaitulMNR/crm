c6770003eb1787cfcbaa224d8fba19fc8f681c8c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-02 11:47:10 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::get('delete/{payment_id}/{product_id}/{package_id}', 'ReportsController@d
 Route::post('updatepayment/{product_id}/{package_id}/{payment_id}/{student_id}', 'ReportsController@updatepayment');
 Route::get('purchased-mail/{product_id}/{package_id}/{payment_id}/{stud_id}', 'ReportsController@purchased_mail');
 Route::post('exportProgram/{product_id}', 'ReportsController@exportProgram');
-Route::get('exportProgram/{product_id}', 'ReportsController@exportProgram');
+// Route::get('exportProgram/{product_id}', 'ReportsController@exportProgram');
 Route::get('customer/search/{product_id}/{package_id}', 'ReportsController@search');
 
 //participant
