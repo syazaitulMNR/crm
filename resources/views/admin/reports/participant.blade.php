67e7dc6446aff30ff30bfd16265125bb5ec2ff0f
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-06 11:40:52 +0800

    update

--- resources/views/admin/reports/participant.blade.php
+++ resources/views/admin/reports/participant.blade.php
@@ -9,7 +9,7 @@
 <div class="col-md-12 pt-3">
         <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
           <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"><i class="bi bi-arrow-left"></i></a> &nbsp; <a href="/dashboard">...</a> 
-          / <a href="/trackprogram">Customer</a> / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"> {{ $product->name }} </a> / <b>{{ $package->name }}</b>
+          / <a href="/trackprogram">Sales Report</a> / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"> {{ $product->name }} </a> / <b>{{ $package->name }}</b>
         </div>
   
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -9,7 +9,7 @@ Sales Report
 <div class="col-md-12 pt-3">     
     <div class="card-header py-2" style="border: 1px solid rgb(233, 233, 233); border-radius: 5px;">
       <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"><i class="bi bi-arrow-left"></i></a> &nbsp; <a href="/dashboard">...</a>
-      / <a href="/trackprogram">Customer</a> / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"> {{ $product->name }} </a> 
+      / <a href="/trackprogram">Sales Report</a> / <a href="{{ url('trackpackage') }}/{{ $product->product_id }}"> {{ $product->name }} </a> 
       / <b>{{ $package->name }}</b>
     </div>
 
