15a38c7070a5cde9af58d0bd0bbe0818ef704196
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-07 11:05:41 +0800

    Update viewbypackage.blade.php

--- resources/views/admin/reports/viewbypackage.blade.php
+++ resources/views/admin/reports/viewbypackage.blade.php
@@ -210,8 +210,8 @@ Sales Report
           <tbody> 
             @foreach ($student as $students) 
             @foreach ($payment as $payments)  
-            @if ($payments->stud_id == $students->stud_id)
-            @if ($payments->product_id == $product->product_id)  
+            @if ($students->stud_id == $payments->stud_id)
+            {{-- @if ($payments->product_id == $product->product_id)   --}}
             <tr>
                 <td>{{ $count++ }}</td>
                 <td>{{ $students->ic }}</td>
@@ -239,7 +239,7 @@ Sales Report
                   <a class="btn btn-dark" href="{{ url('viewpayment') }}/{{ $product->product_id }}/{{ $payments->package_id }}/{{ $payments->payment_id }}/{{ $payments->stud_id }}"><i class="bi bi-chevron-right"></i></a>
                 </td>
             </tr>
-            @endif
+            {{-- @endif --}}
             @endif
             @endforeach
             @endforeach
@@ -264,8 +264,8 @@ Sales Report
         <tbody> 
           @foreach ($student as $students)   
           @foreach ($payment as $payments)
-          @if ($payments->stud_id == $students->stud_id)
-          @if ($payments->product_id == $product->product_id)  
+          @if (($students->stud_id == $payments->stud_id))
+          {{-- @if ($payments->product_id == $product->product_id)   --}}
           <tr>
               <td>{{ $count++ }}</td>
               <td>{{ $students->ic }}</td>
@@ -284,7 +284,7 @@ Sales Report
                 <a class="btn btn-dark" href="{{ url('viewpayment') }}/{{ $product->product_id }}/{{ $payments->package_id }}/{{ $payments->payment_id }}/{{ $payments->stud_id }}"><i class="bi bi-chevron-right"></i></a>
               </td>
           </tr>
-          @endif
+          {{-- @endif --}}
           @endif
           @endforeach
           @endforeach
