1011f95db1b6fa24650fd61ea7bfa44877c7d84c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-09 14:10:46 +0800

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -190,7 +190,7 @@ class AdminController extends Controller
         }
         
         // get the total 
-        $total_today = Carbon::today('Asia/Kuala_Lumpur')->format('d-m-Y');
+        $total_today = Carbon::today('Asia/Kuala_Lumpur');
         $total_now = Carbon::now('Asia/Kuala_Lumpur');
         $todayticket = Ticket::where('product_id', $product_id)->whereBetween('created_at', [ $from , $to ])->count();
 
@@ -201,7 +201,7 @@ class AdminController extends Controller
         $pendingticket = $totalregister - $totalpaid;
         $totalcollection = Payment::where('status','paid')->where('product_id', $product_id)->sum('totalprice');
         
-        return view('admin.dashboard', compact('product', 'package', 'count_package', 'date_today', 'current_time', 'from', 'to', 'duration', 'greetings', 'totalregister', 'totalpaid', 'totalfree', 'totalticket', 'total_now', 'todayticket', 'registration', 'paidticket', 'freeticket', 'totalpackage', 'pendingticket', 'collection', 'totalcollection'));
+        return view('admin.dashboard', compact('product', 'package', 'count_package', 'date_today', 'current_time', 'from', 'to', 'duration', 'greetings', 'totalregister', 'totalpaid', 'totalfree', 'totalticket', 'total_now', 'total_today', 'todayticket', 'registration', 'paidticket', 'freeticket', 'totalpackage', 'pendingticket', 'collection', 'totalcollection'));
     }
 
     /*-- Manage User --------------------------------------------------------*/
