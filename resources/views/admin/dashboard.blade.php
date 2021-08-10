de563f74a66c6b89df42de66b6e950a694ffc27c
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-10 11:47:04 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
   <div class="row pb-2">
     <!-- Show data in table --------------------------------------------------->
     <div class="col-md-8 pb-4">
-      <div class="card bg-white shadow px-4 py-2">
+      <div class="card bg-white shadow px-4 py-3">
 
         <h5 class="text-center py-2">{{ $product->name }}</h5>
 
