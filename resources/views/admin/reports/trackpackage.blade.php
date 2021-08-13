fbc8b88e303e3b2e5c22b343caa5259f5546c414
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-13 11:58:47 +0800

    Update trackpackage.blade.php

--- resources/views/admin/reports/trackpackage.blade.php
+++ resources/views/admin/reports/trackpackage.blade.php
@@ -88,7 +88,7 @@ Sales Report
               <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                   <div class="modal-header border-bottom-0">
-                    <h5 class="modal-title" id="exampleModalLabel">Export Participan</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Export Participant</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
