93fe69c18afbe992a55ffeaa002367679e02b52b
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-16 16:47:58 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -211,10 +211,11 @@ class AdminController extends Controller
         $fri = Carbon::now()->subDays(3)->format('D');
         $sat = Carbon::now()->subDays(2)->format('D');
         $sun = Carbon::now()->subDays(1)->format('D');
+        $day = Carbon::now()->subDays(4)->format('D');
         // $day = Payment::where('product_id', $product_id)->where('status', 'paid')->whereBetween('created_at', [ date('l Y-m-d 00:00:00', strtotime("-1 day")) , date('l Y-m-d 23:59:59', strtotime("-1 day")) ])->count();
-        $day = date('Y-m-d', strtotime('monday'));
+        // $day = date('Y-m-d', strtotime('monday'));
 
-        dd($mon);
+        dd($day);
         // // check duplicate student data --------------------------------//
         // $users = Student::whereIn('stud_id', function ( $query ) {
         //     $query->select('stud_id')->from('student')->groupBy('stud_id')->havingRaw('count(*) > 1');
