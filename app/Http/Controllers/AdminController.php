e0aee87ac99f2718bdebe8e87b8f66a5cbe2177d
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-16 12:33:10 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -205,13 +205,22 @@ class AdminController extends Controller
         $pendingticket = $totalregister - $totalpaid;
         $totalcollection = Payment::where('status','paid')->where('product_id', $product_id)->sum('totalprice');
 
-        $users = Student::whereIn('stud_id', function ( $query ) {
-            $query->select('stud_id')->from('student')->groupBy('stud_id')->havingRaw('count(*) > 1');
+        // $users = Student::whereIn('stud_id', function ( $query ) {
+        //     $query->select('stud_id')->from('student')->groupBy('stud_id')->havingRaw('count(*) > 1');
+        // })->orderBy('id','Desc')->get();
+
+        // foreach ($users as $user) 
+        // {
+        //     echo $user->stud_id . "<br>";
+        // }
+
+        $users = Payment::whereIn('payment_id', function ( $query ) {
+            $query->select('payment_id')->from('payment')->groupBy('payment_id')->havingRaw('count(*) > 1');
         })->orderBy('id','Desc')->get();
 
         foreach ($users as $user) 
         {
-            echo $user->stud_id . "<br>";
+            echo $user->payment_id . "<br>";
         }
         
         // return view('admin.dashboard', compact('product', 'package', 'count_package', 'date_today', 'current_time', 'from', 'to', 'duration', 'greetings', 'totalregister', 'totalpaid', 'totalfree', 'totalticket', 'total_now', 'total_yesterday', 'registration', 'paidticket', 'freeticket', 'totalpackage', 'pendingticket', 'collection', 'totalcollection'));
