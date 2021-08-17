b32a9139e15f5787be002f1f17e0c182115d0df2
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-17 15:10:39 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -235,8 +235,8 @@
     },
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="padding:3">RM </td>' +
-            '<td style="padding:3"><b> {point.y:.2f} </b></td></tr>',
+        pointFormat: '<tr><td style="padding:3"> </td>' +
+            '<td style="padding:3"><b>  </b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
