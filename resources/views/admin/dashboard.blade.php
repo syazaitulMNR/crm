942130af14fa483c9e711a41c62ec46a6320b621
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-18 09:43:33 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -202,8 +202,7 @@
   <div class="card border-0 shadow text-center text-danger pt-3" style="height: 272px"> 
     <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
 
-    <p>Updated Ticket : {{ number_format($totalticket) }}</p>
-    <p>Pending Ticket : {{ number_format($pendingticket) }}</p>
+    <p>Updated Ticket : <b>{{ number_format($totalticket) }}</b>  Pending Ticket : <b>{{ number_format($pendingticket) }}</b></p>
   </div>
   <br>
   <div class="card border-0 gradient-3 shadow text-center" style="height: 136px">
