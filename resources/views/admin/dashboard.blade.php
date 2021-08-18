c3878e2a846d68c0df40f88c554f083c82830acb
Nrzrnhzkria <69852327+rndmomentum@users.noreply.github.com>
2021-08-18 12:10:42 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -38,7 +38,7 @@
     <!-- Show data in bar chart ----------------------------------------------->
     <div class="col-md-4 pb-4">
       <div class="card border-0 bg-white shadow px-4 py-4">      
-        <canvas id="barChart" style="width:100%; height: 400px"></canvas>
+        <canvas id="barChart" style="width:100%; height: 405px"></canvas>
       </div>
     </div>
     
