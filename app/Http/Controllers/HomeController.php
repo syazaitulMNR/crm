0fb863349741aecbe886b7ee7fcd0d1dcebbb08b
rndmomentum <69852327+rndmomentum@users.noreply.github.com>
2021-09-15 09:57:50 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\RedirectResponse;
 use App\Jobs\TiketJob;
 use App\Product;
 use App\Feature;
