9ec503ef752e17e276b574c86930607da932c1be
syhrulhkim <89004999+syhrulhkim@users.noreply.github.com>
2022-01-26 13:45:17 +0800

    Update businessDetail.blade.php

--- resources/views/ticket/businessDetail.blade.php
+++ resources/views/ticket/businessDetail.blade.php
@@ -69,7 +69,6 @@
                             </select>
                         </div>
                     </div>
-                    
                     <button type="submit" class="btn btn-primary float-end"><i class="bi bi-save pr-2"></i> Submit</button>
                 </form>
             </div>
@@ -92,7 +91,7 @@
 </script>
 
 <footer class="text-center px-4 py-5">
-    <b>Momentum Internet (1079998-A) © 2020 All Rights Reserved​</b>
+    <b>Momentum Internet (1079998-A) © 2022 All Rights Reserved​</b>
 </footer>
 
 @endsection
