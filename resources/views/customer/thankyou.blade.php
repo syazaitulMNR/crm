fb695a9cd02cea3d92f6012f709383964fb63912
syhrulhkim <89004999+syhrulhkim@users.noreply.github.com>
2022-04-12 12:24:20 +0800

    Update web not secure

--- resources/views/customer/thankyou.blade.php
+++ resources/views/customer/thankyou.blade.php
@@ -9,23 +9,23 @@
     @if ($value->package_id == "PKD0082" || $value->package_id == "PKD0083" )
     <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbjohor/"> 
     @elseif ($value->package_id == "PKD0084" || $value->package_id == "PKD0085")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbkl/"> 
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbkl/"> 
     @elseif ($value->package_id == "PKD0086" || $value->package_id == "PKD0088")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbsp/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbsp/">
     @elseif ($value->package_id == "PKD0091" || $value->package_id == "PKD0092")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbsa/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbsa/">
     @elseif ($value->package_id == "PKD0093" || $value->package_id == "PKD0094")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbkb/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbkb/">
     @elseif ($value->package_id == "PKD0095" || $value->package_id == "PKD0096")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbktn/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbktn/">
     @elseif ($value->package_id == "PKD0097" || $value->package_id == "PKD0098")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbsbh/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbsbh/">
     @elseif ($value->package_id == "PKD0099" || $value->package_id == "PKD00100")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbmlk/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbmlk/">
     @elseif ($value->package_id == "PKD00101" || $value->package_id == "PKD00102")
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mbjb/">  
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mbjb/">  
     @else 
-    <meta http-equiv="refresh" content="2;url=https://www.momentumbisnes.com/oto-mb/">
+    <meta http-equiv="refresh" content="2;url=http://www.momentumbisnes.com/oto-mb/">
     @endif  
   @endforeach
 </head>
