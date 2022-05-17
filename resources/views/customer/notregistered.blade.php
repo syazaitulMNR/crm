eb47a4b23a8375b032a81d85d7cf00a3bf88564c
syhrulhkim <89004999+syhrulhkim@users.noreply.github.com>
2022-05-17 09:48:32 +0800

    Update contact us

--- resources/views/customer/notregistered.blade.php
+++ resources/views/customer/notregistered.blade.php
@@ -15,8 +15,8 @@ Pendaftaran Pembeli
 <div class="row">
     <div class="col-md-12 px-5 py-4">
         <div class="text-center">
-            <h3 class="display-4">Harap Maaf!</h3>
-            <h3 class="display-4">Proses pembayaran telah dibatalkan/tidak berjaya.</h3>
+            <h3 class="display-4">Tidak Berdaftar!</h3>
+            <h3 class="display-4">Tiada rekod untuk pembayaran bagi peserta!</h3>
             <div class="py-3" style="font-size: 24px; color: red;">
             <i class="far fa-times-circle fa-8x text-center"></i>
             </div>
