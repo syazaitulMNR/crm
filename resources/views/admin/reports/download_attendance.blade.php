911baa3a975b3aa19805e648696b43248eae0f82
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2022-08-03 16:16:00 +0800

    fix: error download attendance

--- resources/views/admin/reports/download_attendance.blade.php
+++ resources/views/admin/reports/download_attendance.blade.php
@@ -6,41 +6,23 @@
             <th>ic</th>
             <th>email</th>
             <th>phoneno</th>
-            <th>name</th>
+            {{-- <th>name</th> --}}
         </tr>
     </thead>
     <tbody>
-            @foreach ($studentpay as $keystud => $valpay) 
-                @foreach ($valpay as $datastud) 
-                <tr>
-                    <td>{{ $datastud->first_name }}</td>
-                    <td>{{ $datastud->last_name }}</td>
-                    <td>{{ $datastud->ic }}</td>
-                    <td>{{ $datastud->phoneno }}</td>
-                    <td>{{ $datastud->email }}</td>
-                    <td>{{ $product->name }}</td>
-                </tr>    
-                @endforeach
-            @endforeach
-
-            @if (count($ticket) >= 1)
-                @foreach ($studenttic as $keytic => $valtic)
-                    @foreach ($valtic as $datatic) 
+        @foreach ($payment as $pay) 
+            @foreach ($student as $stud) 
+                @if ($pay->stud_id == $stud->stud_id)
                     <tr>
-                        <td>{{ $datatic->first_name }}</td>
-                        <td>{{ $datatic->last_name }}</td>
-                        <td>{{ $datatic->ic }}</td>
-                        <td>{{ $datatic->phoneno }}</td>
-                        <td>{{ $datatic->email }}</td>
-                        <td>{{ $product->name }}</td>
-                    </tr>    
-                    @endforeach
-                @endforeach
-            @endif
-            {{-- <td>John</td>
-            <td>Doe</td>
-            <td>912345006789</td>
-            <td>example@gmail.com</td>
-            <td>+60123456789</td>
-            <td>+60123456789</td> --}}
-</tbody>
+                        <td>{{ $stud->first_name }}</td>
+                        <td>{{ $stud->last_name }}</td>
+                        <td>{{ $stud->ic }}</td>
+                        <td>{{ $stud->email }}</td>
+                        <td>{{ $stud->phoneno }}</td>
+                        {{-- <td>{{ $product->name }}</td> --}}
+                    </tr>   
+                @endif 
+            @endforeach
+        @endforeach
+    </tbody>
+</table>
